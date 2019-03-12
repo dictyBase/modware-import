215ce799469a654622c9c79cf510f37a862acd5b
Siddhartha Basu <biosidd@gmail.com>
2019-03-12 14:57:05 +0000

    feat: add csv datasource for reading rows of data

--- internal/datasource/csv/csv.go
+++ internal/datasource/csv/csv.go
@@ -1,1 +1,32 @@
+//package csv is for managing data from csv file
 package csv
+
+import (
+	"encoding/csv"
+	"io"
+)
+
+// CsvReader is to manage record from csv file
+type CsvReader struct {
+	Reader *csv.Reader
+	Record []string
+	Err    error
+}
+
+// Next read the next csv record
+func (r *CsvReader) Next() bool {
+	r.Err = nil
+	record, err := r.Reader.Read()
+	if err == io.EOF {
+		return false
+	}
+	if err != nil {
+		r.Err = err
+		return true
+	}
+	if len(r.Record) > 1 {
+		r.Record = nil
+	}
+	r.Record = append(r.Record, record...)
+	return true
+}
--- internal/datasource/datasource.go
+++ internal/datasource/datasource.go
@@ -1,5 +1,7 @@
+//package datasource is for managing data from different source
 package datasource
 
+// IteratorWithoutValue is an interface for iteration only
 type IteratorWithoutValue interface {
 	Next() bool
 }
