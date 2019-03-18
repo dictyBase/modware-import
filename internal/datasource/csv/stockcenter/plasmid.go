005d8a21f76880dab75468b30c1391a2797453dc
Siddhartha Basu <biosidd@gmail.com>
2019-03-18 19:45:57 +0000

    feat: add csv datasource for reading links between plasmid and genes

--- internal/datasource/csv/stockcenter/plasmid.go
+++ internal/datasource/csv/stockcenter/plasmid.go
@@ -24,7 +24,7 @@ type csvPlasmidGenbankReader struct {
 	*csource.CsvReader
 }
 
-//NewPlasmidReader is to get an instance of PlasmidGenbankReader
+//NewPlasmidGenbankReader is to get an instance of PlasmidGenbankReader
 func NewPlasmidGenbankReader(r io.Reader) PlasmidGenbankReader {
 	cr := csv.NewReader(r)
 	cr.FieldsPerRecord = -1
@@ -41,4 +41,39 @@ func (pgr *csvPlasmidGenbankReader) Value() (*PlasmidGenbank, error) {
 	g.Id = pgr.Record[0]
 	g.Genbank = pgr.Record[1]
 	return g, nil
+}
+
+//PlasmidGene is the container for plasmid and gene identifier links
+type PlasmidGene struct {
+	Id     string
+	GeneId string
+}
+
+//PlasmidGene is the defined interface for reading the data
+type PlasmidGenbankReader interface {
+	datasource.IteratorWithoutValue
+	Value() (*PlasmidGene, error)
+}
+
+type csvPlasmidGeneReader struct {
+	*csource.CsvReader
+}
+
+//NewPlasmidGeneReader is to get an instance of PlasmidGeneReader
+func NewPlasmidGeneReader(r io.Reader) PlasmidGeneReader {
+	cr := csv.NewReader(r)
+	cr.FieldsPerRecord = -1
+	cr.Comma = '\t'
+	return &csvPlasmidGeneReader{&csource.CsvReader{Reader: cr}}
+}
+
+//Value gets a new PlasmidGene instance
+func (pger *csvPlasmidGeneReader) Value() (*PlasmidGene, error) {
+	gene := new(PlasmidGene)
+	if pger.Err != nil {
+		return gene, pger.Err
+	}
+	gene.Id = pger.Record[0]
+	gene.GeneId = pger.Record[1]
+	return gene, nil
 }
