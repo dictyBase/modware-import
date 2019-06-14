7b600d6492ca0b0f0d26b5e37f3f92c27a4b4ff4
Siddhartha Basu <biosidd@gmail.com>
2019-06-13 19:11:01 -0500

    refactor: move findOrCreateAnno method to a new file

---
+++ internal/load/stockcenter/stockcenter.go
@@ -0,0 +1,47 @@
+package stockcenter
+
+import (
+	"context"
+	"fmt"
+
+	pb "github.com/dictyBase/go-genproto/dictybaseapis/annotation"
+	regs "github.com/dictyBase/modware-import/internal/registry/stockcenter"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+)
+
+func findOrCreateAnno(client pb.TaggedAnnotationServiceClient, tag, id, ontology, value string) (*pb.TaggedAnnotation, error) {
+	ta, err := client.GetEntryAnnotation(
+		context.Background(),
+		&pb.EntryAnnotationRequest{
+			Tag:      tag,
+			EntryId:  id,
+			Ontology: ontology,
+		})
+	switch {
+	case err == nil:
+		return ta, nil
+	case grpc.Code(err) == codes.NotFound:
+		return client.CreateAnnotation(
+			context.Background(),
+			&pb.NewTaggedAnnotation{
+				Data: &pb.NewTaggedAnnotation_Data{
+					Attributes: &pb.NewTaggedAnnotationAttributes{
+						Value:     value,
+						CreatedBy: regs.DEFAULT_USER,
+						Tag:       tag,
+						EntryId:   id,
+						Ontology:  ontology,
+					},
+				},
+			},
+		)
+
+	}
+	return ta, fmt.Errorf(
+		"error in finding annotation %s for id %s %s",
+		tag,
+		id,
+		err,
+	)
+}
