853f5c11865581c57a638944cbe512047b1fe040
Siddhartha Basu <biosidd@gmail.com>
2019-06-19 18:30:29 -0500

    refactor: move all constants to a separate file

--- internal/registry/stockcenter/stockcenter.go
+++ internal/registry/stockcenter/stockcenter.go
@@ -7,31 +7,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-const (
-	ORDER_CLIENT_KEY           = "order-client"
-	STOCK_CLIENT_KEY           = "stock-client"
-	ANNOTATION_CLIENT_KEY      = "annotation-client"
-	PLASMID_ID_MAP_READER      = "plasmid-id-map-input"
-	PLASMID_ANNOTATOR_READER   = "plasmid-annotator-input"
-	PLASMID_PUB_READER         = "plasmid-pub-input"
-	PLASMID_GENE_READER        = "plasmid-gene-input"
-	PLASMID_READER             = "plasmid-input"
-	ORDER_READER               = "order-input"
-	STRAIN_READER              = "strain-input"
-	STRAIN_ANNOTATOR_READER    = "strain-annotator-input"
-	STRAIN_PUB_READER          = "strain-pub-input"
-	STRAIN_GENE_READER         = "strain-gene-input"
-	STRAINCHAR_READER          = "strain-char-input"
-	PHENO_READER               = "pheno-input"
-	GENO_READER                = "geno-input"
-	INV_READER                 = "inv-input"
-	STRAINPROP_READER          = "strainprop-input"
-	DEFAULT_USER               = "dictybase@northwestern.edu"
-	DICTY_ANNO_ONTOLOGY        = "dicty_annotation"
-	DICTY_MUTAGENESIS_ONTOLOGY = "Dd Mutagenesis Method"
-	DICTY_GENETICMOD_ONTOLOGY  = "genetic modification"
-)
-
 var sv = viper.New()
 
 func SetOrderAPIClient(oc order.OrderServiceClient) {
