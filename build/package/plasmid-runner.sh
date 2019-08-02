f1d75f5876e69330a705ab0aa2228e69faf6920d
Siddhartha Basu <biosidd@gmail.com>
2019-08-02 11:20:19 -0500

    refactor: add for loop to run subcommand serially

--- build/package/plasmid-runner.sh
+++ build/package/plasmid-runner.sh
@@ -1,19 +1,23 @@
 #!/bin/sh 
 
 APP=$(which app)
-${APP} stockcenter \
-    plasmid \
-    --access-key ${ACCESS_KEY} \
-    --secret-key  ${SECRET_KEY} \
-    --log-level debug \
-    -a plasmid_user_annotations.csv \
-    -g plasmid_genes.tsv \
-    -i plasmid_plasmid.tsv \
-    -p plasmid_publications.tsv
 
-${APP} stockcenter \
-    strainchar \
-    --access-key ${ACCESS_KEY} \
-    --secret-key  ${SECRET_KEY} \
-    --log-level debug \
-    -i strain_characteristics.tsv
+for scmd in "$@"
+do
+    case $scmd in
+        "plasmid")
+            ${APP} stockcenter \
+                plasmid \
+                --access-key ${ACCESS_KEY} \
+                --secret-key  ${SECRET_KEY} \
+                --log-level debug \
+                -a plasmid_user_annotations.csv \
+                -g plasmid_genes.tsv \
+                -i plasmid_plasmid.tsv \
+                -p plasmid_publications.tsv
+            ;;
+        "*")
+                echo unknown command $scmd
+            ;;
+    esac
+done
--- build/package/strain-runner.sh
+++ build/package/strain-runner.sh
@@ -1,19 +1,31 @@
 #!/bin/sh 
 
 APP=$(which app)
-${APP} stockcenter \
-    strain \
-    --access-key ${ACCESS_KEY} \
-    --secret-key  ${SECRET_KEY} \
-    --log-level debug \
-    -a strain_user_annotations.csv \
-    -g strain_genes.tsv \
-    -i strain_strain.tsv \
-    -p strain_publications.tsv
 
-${APP} stockcenter \
-    strainchar \
-    --access-key ${ACCESS_KEY} \
-    --secret-key  ${SECRET_KEY} \
-    --log-level debug \
-    -i strain_characteristics.tsv
+for scmd in "$@"
+do
+    case $scmd in
+        "strain")
+            ${APP} stockcenter \
+                strain \
+                --access-key ${ACCESS_KEY} \
+                --secret-key  ${SECRET_KEY} \
+                --log-level debug \
+                -a strain_user_annotations.csv \
+                -g strain_genes.tsv \
+                -i strain_strain.tsv \
+                -p strain_publications.tsv
+            ;;
+        "characteristics")
+                ${APP} stockcenter \
+                    strainchar \
+                    --access-key ${ACCESS_KEY} \
+                    --secret-key  ${SECRET_KEY} \
+                    --log-level debug \
+                    -i strain_characteristics.tsv
+            ;;
+        "*")
+                echo unknown command $scmd
+            ;;
+    esac
+done
