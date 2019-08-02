63f96cd2f306615d57c6a2d13f662b26cbf364c2
Siddhartha Basu <biosidd@gmail.com>
2019-08-02 15:42:51 -0500

    refactor: use a single script to load all data files

--- build/package/plasmid-runner.sh
+++ build/package/plasmid-runner.sh
@@ -5,25 +5,6 @@ APP=$(which app)
 for scmd in "$@"
 do
     case $scmd in
-        "strain")
-            ${APP} stockcenter \
-                strain \
-                --access-key ${ACCESS_KEY} \
-                --secret-key  ${SECRET_KEY} \
-                --log-level debug \
-                -a strain_user_annotations.csv \
-                -g strain_genes.tsv \
-                -i strain_strain.tsv \
-                -p strain_publications.tsv
-            ;;
-        "characteristics")
-                ${APP} stockcenter \
-                    strainchar \
-                    --access-key ${ACCESS_KEY} \
-                    --secret-key  ${SECRET_KEY} \
-                    --log-level debug \
-                    -i strain_characteristics.tsv
-            ;;
         "plasmid")
             ${APP} stockcenter \
                 plasmid \
@@ -35,7 +16,7 @@ do
                 -i plasmid_plasmid.tsv \
                 -p plasmid_publications.tsv
             ;;
-        "*")
+        *)
                 echo unknown command $scmd
             ;;
     esac
