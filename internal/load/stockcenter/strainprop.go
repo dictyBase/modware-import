4047cd3acc88a10d3e270399a37de59ddd3cbd5c
Siddhartha Basu <biosidd@gmail.com>
2019-08-19 16:41:31 -0500

    refactor: remove and reload all synonyms

--- internal/load/stockcenter/strainprop.go
+++ internal/load/stockcenter/strainprop.go
@@ -1,8 +1,13 @@
 package stockcenter
 
 import (
+	"context"
 	"fmt"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+
+	"github.com/dictyBase/go-genproto/dictybaseapis/annotation"
 	"github.com/dictyBase/modware-import/internal/datasource/csv/stockcenter"
 	"github.com/dictyBase/modware-import/internal/registry"
 	regs "github.com/dictyBase/modware-import/internal/registry/stockcenter"
@@ -21,6 +26,7 @@ func LoadStrainProp(cmd *cobra.Command, args []string) error {
 	client := regs.GetAnnotationAPIClient()
 	logger := registry.GetLogger()
 	pcount := 0
+	synMap := make(map[string][]*stockcenter.StockProp)
 	for pr.Next() {
 		prop, err := pr.Value()
 		if err != nil {
@@ -38,7 +44,13 @@ func LoadStrainProp(cmd *cobra.Command, args []string) error {
 		case mutmethodTag:
 			onto = regs.DICTY_MUTAGENESIS_ONTOLOGY
 		case synTag:
-			onto = regs.DICTY_ANNO_ONTOLOGY
+			// cache all synonyms
+			if _, ok := synMap[prop.Id]; !ok {
+				synMap[prop.Id] = []*stockcenter.StockProp{prop}
+			} else {
+				synMap[prop.Id] = append(synMap[prop.Id], prop)
+			}
+			continue
 		default:
 			logger.Warnf(
 				"property %s is not recognized, record is not loaded",
@@ -55,6 +67,56 @@ func LoadStrainProp(cmd *cobra.Command, args []string) error {
 			prop.Id, prop.Property, prop.Value,
 		)
 		pcount++
+	}
+	// load all the synonyms
+	for entryId, props := range synMap {
+		tac, err := client.ListAnnotations(
+			context.Background(),
+			&annotation.ListParameters{
+				Limit: 20,
+				Filter: fmt.Sprintf(
+					"entry_id==%s;tag==%s;ontology==%s",
+					entryId, synTag, regs.DICTY_ANNO_ONTOLOGY,
+				)})
+		if err != nil {
+			if grpc.Code(err) != codes.NotFound {
+				return fmt.Errorf("error in listing synonyms for %s %s", entryId, err)
+			}
+		} else {
+			// remove synonyms
+			for _, ta := range tac.Data {
+				_, err := client.DeleteAnnotation(
+					context.Background(),
+					&annotation.DeleteAnnotationRequest{
+						EntryId: ta.Id,
+						Purge:   true,
+					})
+				if err != nil {
+					return err
+				}
+			}
+		}
+		// reload all synonyms
+		for i, p := range props {
+			_, err := client.CreateAnnotation(
+				context.Background(),
+				&annotation.NewTaggedAnnotation{
+					Data: &annotation.NewTaggedAnnotation_Data{
+						Attributes: &annotation.NewTaggedAnnotationAttributes{
+							Value:     p.Value,
+							CreatedBy: regs.DEFAULT_USER,
+							Tag:       synTag,
+							EntryId:   entryId,
+							Ontology:  regs.DICTY_ANNO_ONTOLOGY,
+							Rank:      int64(i),
+						},
+					},
+				})
+			if err != nil {
+				return fmt.Errorf("unable to load synonym %s for %s %s", p.Value, entryId, err)
+			}
+		}
+		logger.Debugf("loaded all strain synonyms for %s", entryId)
 	}
 	logger.Infof("loaded %d strain properties", pcount)
 	return nil
