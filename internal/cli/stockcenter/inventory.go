2dd8dffa08a56c7b34296cf2011262a9b6a448e7
Siddhartha Basu <biosidd@gmail.com>
2019-10-16 18:51:53 -0500

    feat: refactor common method into inventory.go file

---
+++ internal/cli/stockcenter/inventory.go
@@ -0,0 +1,73 @@
+package stockcenter
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/dictyBase/modware-import/internal/registry"
+	regsc "github.com/dictyBase/modware-import/internal/registry/stockcenter"
+	minio "github.com/minio/minio-go"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func setInvPreRun(cmd *cobra.Command, args []string) error {
+	if err := setAnnoAPIClient(); err != nil {
+		return err
+	}
+	if err := setInvInputReader(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func setInvInputReader() error {
+	switch viper.GetString("input-source") {
+	case "folder":
+		pr, err := os.Open(viper.GetString("inventory-input"))
+		if err != nil {
+			return fmt.Errorf("error in opening file %s %s", viper.GetString("inventory-input"), err)
+		}
+		registry.SetReader(regsc.INV_READER, pr)
+	case "bucket":
+		ar, err := registry.GetS3Client().GetObject(
+			viper.GetString("s3-bucket-path"),
+			viper.GetString("inventory-input"),
+			minio.GetObjectOptions{},
+		)
+		if err != nil {
+			return fmt.Errorf(
+				"error in getting file %s from bucket %s %s",
+				viper.GetString("inventory-input"),
+				viper.GetString("s3-bucket-path"),
+				err,
+			)
+		}
+		registry.SetReader(regsc.INV_READER, ar)
+	default:
+		return fmt.Errorf("error input source %s not supported", viper.GetString("input-source"))
+	}
+	return nil
+}
+
+func initInvCmd(cmd *cobra.Command) {
+	cmd.Flags().String(
+		"annotation-grpc-host",
+		"annotation-api",
+		"grpc host address for annotation service",
+	)
+	viper.BindEnv("annotation-grpc-host", "ANNOTATION_API_SERVICE_HOST")
+	cmd.Flags().String(
+		"annotation-grpc-port",
+		"",
+		"grpc port for annotation service",
+	)
+	viper.BindEnv("annotation-grpc-port", "ANNOTATION_API_SERVICE_PORT")
+	cmd.Flags().StringP(
+		"inventory-input",
+		"i",
+		"",
+		"csv file with inventory data",
+	)
+	viper.BindPFlags(cmd.Flags())
+}
--- internal/cli/stockcenter/strain_inventory.go
+++ internal/cli/stockcenter/strain_inventory.go
@@ -1,83 +1,19 @@
 package stockcenter
 
 import (
-	"fmt"
-	"os"
-
 	loader "github.com/dictyBase/modware-import/internal/load/stockcenter"
-	"github.com/dictyBase/modware-import/internal/registry"
-	regsc "github.com/dictyBase/modware-import/internal/registry/stockcenter"
-	minio "github.com/minio/minio-go"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-// InvCmd is for loading stockcenter inventory data
-var InvCmd = &cobra.Command{
-	Use:     "inventory",
-	Short:   "load stockcenter inventory data",
+// StrainInvCmd is for loading strain inventory data
+var StrainInvCmd = &cobra.Command{
+	Use:     "strain-inventory",
+	Short:   "load strain inventory data",
 	Args:    cobra.NoArgs,
-	RunE:    loader.LoadInv,
+	RunE:    loader.LoadStrainInv,
 	PreRunE: setInvPreRun,
 }
 
-func setInvPreRun(cmd *cobra.Command, args []string) error {
-	if err := setAnnoAPIClient(); err != nil {
-		return err
-	}
-	if err := setInvInputReader(); err != nil {
-		return err
-	}
-	return nil
-}
-
-func setInvInputReader() error {
-	switch viper.GetString("input-source") {
-	case "folder":
-		pr, err := os.Open(viper.GetString("inventory-input"))
-		if err != nil {
-			return fmt.Errorf("error in opening file %s %s", viper.GetString("inventory-input"), err)
-		}
-		registry.SetReader(regsc.INV_READER, pr)
-	case "bucket":
-		ar, err := registry.GetS3Client().GetObject(
-			viper.GetString("s3-bucket-path"),
-			viper.GetString("inventory-input"),
-			minio.GetObjectOptions{},
-		)
-		if err != nil {
-			return fmt.Errorf(
-				"error in getting file %s from bucket %s %s",
-				viper.GetString("inventory-input"),
-				viper.GetString("s3-bucket-path"),
-				err,
-			)
-		}
-		registry.SetReader(regsc.INV_READER, ar)
-	default:
-		return fmt.Errorf("error input source %s not supported", viper.GetString("input-source"))
-	}
-	return nil
-}
-
 func init() {
-	InvCmd.Flags().String(
-		"annotation-grpc-host",
-		"annotation-api",
-		"grpc host address for annotation service",
-	)
-	viper.BindEnv("annotation-grpc-host", "ANNOTATION_API_SERVICE_HOST")
-	InvCmd.Flags().String(
-		"annotation-grpc-port",
-		"",
-		"grpc port for annotation service",
-	)
-	viper.BindEnv("annotation-grpc-port", "ANNOTATION_API_SERVICE_PORT")
-	InvCmd.Flags().StringP(
-		"inventory-input",
-		"i",
-		"",
-		"csv file with inventory data",
-	)
-	viper.BindPFlags(InvCmd.Flags())
+	initInvCmd(StrainInvCmd)
 }
