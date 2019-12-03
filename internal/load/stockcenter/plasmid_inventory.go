43ed525923372c7a67fbdce407a952150e16b874
Siddhartha Basu <biosidd@gmail.com>
2019-12-02 18:03:00 -0600

    refactor: add rank and individual tag for loading plasmid inventory

--- internal/load/stockcenter/plasmid_inventory.go
+++ internal/load/stockcenter/plasmid_inventory.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	pb "github.com/dictyBase/go-genproto/dictybaseapis/annotation"
-	"github.com/dictyBase/modware-import/internal/datasource/csv/stockcenter"
+	"github.com/dictyBase/modware-import/internal/datasource/tsv/stockcenter"
 	"github.com/dictyBase/modware-import/internal/registry"
 	regs "github.com/dictyBase/modware-import/internal/registry/stockcenter"
 	"github.com/sirupsen/logrus"
@@ -16,7 +16,7 @@ import (
 )
 
 func LoadPlasmidInv(cmd *cobra.Command, args []string) error {
-	ir := stockcenter.NewCsvPlasmidInventoryReader(registry.GetReader(regs.INV_READER))
+	ir := stockcenter.NewTsvPlasmidInventoryReader(registry.GetReader(regs.INV_READER))
 	logger := registry.GetLogger()
 	invMap, err := cacheInvByPlasmidId(ir, logger)
 	if err != nil {
@@ -32,6 +32,13 @@ func LoadPlasmidInv(cmd *cobra.Command, args []string) error {
 				return err
 			}
 			found = false
+			logger.WithFields(
+				logrus.Fields{
+					"type":  "inventory",
+					"stock": "plasmid",
+					"event": "get",
+					"id":    id,
+				}).Debugf("no inventories")
 		}
 		if found {
 			logger.WithFields(
@@ -52,7 +59,7 @@ func LoadPlasmidInv(cmd *cobra.Command, args []string) error {
 					"id":    id,
 				}).Debugf("deleted inventories")
 		}
-		if err := createPlasmidInventory(id, client, invSlice); err != nil {
+		if err := createPlasmidInventory(id, client, invSlice, found); err != nil {
 			return err
 		}
 		logger.WithFields(
@@ -95,8 +102,8 @@ func cacheInvByPlasmidId(ir stockcenter.PlasmidInventoryReader, logger *logrus.E
 				}).Warnf("skipped the record")
 			continue
 		}
-		if v, ok := invMap[inv.PlasmidId]; ok {
-			invMap[inv.PlasmidId] = append(v, inv)
+		if invSlice, ok := invMap[inv.PlasmidId]; ok {
+			invMap[inv.PlasmidId] = append(invSlice, inv)
 			continue
 		}
 		invMap[inv.PlasmidId] = []*stockcenter.PlasmidInventory{inv}
@@ -104,8 +111,8 @@ func cacheInvByPlasmidId(ir stockcenter.PlasmidInventoryReader, logger *logrus.E
 	return invMap, nil
 }
 
-func createPlasmidInventory(id string, client pb.TaggedAnnotationServiceClient, invSlice []*stockcenter.PlasmidInventory) error {
-	for _, inv := range invSlice {
+func createPlasmidInventory(id string, client pb.TaggedAnnotationServiceClient, invSlice []*stockcenter.PlasmidInventory, found bool) error {
+	for i, inv := range invSlice {
 		var ids []string
 		m := map[string]string{
 			regs.INV_LOCATION_TAG:     inv.PhysicalLocation,
@@ -117,20 +124,32 @@ func createPlasmidInventory(id string, client pb.TaggedAnnotationServiceClient, 
 		if !inv.StoredOn.IsZero() {
 			m[regs.INV_STORAGE_DATE_TAG] = inv.StoredOn.Format(time.RFC3339Nano)
 		}
-		for t, v := range m {
-			if len(v) == 0 {
-				continue
+	INNER:
+		for tag, value := range m {
+			if len(value) == 0 {
+				continue INNER
 			}
-			a, err := createAnno(client, t, inv.PlasmidId, regs.PLASMID_INV_ONTO, v)
+			anno, err := createAnnoWithRank(client, tag, inv.PlasmidId, regs.PLASMID_INV_ONTO, value, i)
 			if err != nil {
 				return err
 			}
-			ids = append(ids, a.Data.Id)
+			ids = append(ids, anno.Data.Id)
 		}
 		_, err := client.CreateAnnotationGroup(context.Background(), &pb.AnnotationIdList{Ids: ids})
 		if err != nil {
 			return err
 		}
+	}
+	// create presence of inventory annotation
+	if !found {
+		_, err := createAnno(
+			client, regs.PLASMID_INV_ONTO, invSlice[0].PlasmidId,
+			regs.PLASMID_INV_ONTO, regs.INV_EXIST_VALUE,
+		)
+		if err != nil {
+			return err
+		}
+
 	}
 	return nil
 }
