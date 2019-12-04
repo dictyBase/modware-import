bab428e8d43f9cd5e677bdf8ea3982679e0a3a2a
Siddhartha Basu <biosidd@gmail.com>
2019-12-03 20:51:21 -0600

    chore: remove one debugging comment

--- internal/datasource/s3/client.go
+++ internal/datasource/s3/client.go
@@ -9,7 +9,6 @@ import (
 )
 
 func NewS3Client(cmd *cobra.Command) (*minio.Client, error) {
-	fmt.Println(viper.GetString("s3-server"))
 	return minio.New(
 		fmt.Sprintf("%s:%s", viper.GetString("s3-server"), viper.GetString("s3-server-port")),
 		viper.GetString("access-key"),
