43dd54793e58510d262ffea81c3aaab8d93edf5f
Siddhartha Basu <biosidd@gmail.com>
2019-12-17 13:34:31 -0600

    refactor: split NewLogger method in two smaller methods

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -12,11 +12,61 @@ import (
 )
 
 func NewLogger(cmd *cobra.Command) (*logrus.Entry, error) {
-	e := new(logrus.Entry)
-	logger := logrus.New()
-	logger.SetOutput(os.Stderr)
-	format, _ := cmd.Flags().GetString("log-format")
+	lfmt, err := getLogFmt(cmd)
+	if err != nil {
+		return &logrus.Entry{}, err
+	}
+	level, err := getLogLevel(cmd)
+	if err != nil {
+		return &logrus.Entry{}, err
+	}
+	logger := &logrus.Logger{
+		Out:       os.Stderr,
+		Formatter: lfmt,
+		Level:     level,
+	}
+	// set hook to write to local file
+	fname, _ := cmd.Flags().GetString("log-file")
+	if len(fname) == 0 {
+		f, err := ioutil.TempFile(os.TempDir(), "loader")
+		if err != nil {
+			return &logrus.Entry{}, fmt.Errorf("error in creating temp file for logging %s", err)
+		}
+		fname = f.Name()
+	}
+	logger.Hooks.Add(lfshook.NewHook(fname, lfmt))
+	registry.SetValue(registry.LOG_FILE_KEY, fname)
+	return logrus.NewEntry(logger), nil
+}
+
+func getLogLevel(cmd *cobra.Command) (logrus.Level, error) {
+	var level logrus.Level
+	name, _ := cmd.Flags().GetString("log-level")
+	switch name {
+	case "debug":
+		level = logrus.DebugLevel
+	case "info":
+		level = logrus.InfoLevel
+	case "warn":
+		level = logrus.WarnLevel
+	case "error":
+		level = logrus.ErrorLevel
+	case "fatal":
+		level = logrus.FatalLevel
+	case "panic":
+		level = logrus.PanicLevel
+	default:
+		return level, fmt.Errorf(
+			"%s log level is not supported",
+			level,
+		)
+	}
+	return level, nil
+}
+
+func getLogFmt(cmd *cobra.Command) (logrus.Formatter, error) {
 	var lfmt logrus.Formatter
+	format, _ := cmd.Flags().GetString("log-format")
 	switch format {
 	case "text":
 		lfmt = &logrus.TextFormatter{
@@ -27,42 +77,10 @@ func NewLogger(cmd *cobra.Command) (*logrus.Entry, error) {
 			TimestampFormat: "02/Jan/2006:15:04:05",
 		}
 	default:
-		return e, fmt.Errorf(
+		return lfmt, fmt.Errorf(
 			"only json and text are supported %s log format is not supported",
 			format,
 		)
 	}
-	logger.SetFormatter(lfmt)
-	level, _ := cmd.Flags().GetString("log-level")
-	switch level {
-	case "debug":
-		logger.SetLevel(logrus.DebugLevel)
-	case "info":
-		logger.SetLevel(logrus.InfoLevel)
-	case "warn":
-		logger.SetLevel(logrus.WarnLevel)
-	case "error":
-		logger.SetLevel(logrus.ErrorLevel)
-	case "fatal":
-		logger.SetLevel(logrus.FatalLevel)
-	case "panic":
-		logger.SetLevel(logrus.PanicLevel)
-	default:
-		return e, fmt.Errorf(
-			"%s log level is not supported",
-			level,
-		)
-	}
-	// set hook to write to local file
-	fname, _ := cmd.Flags().GetString("log-file")
-	if len(fname) == 0 {
-		f, err := ioutil.TempFile(os.TempDir(), "loader")
-		if err != nil {
-			return e, fmt.Errorf("error in creating temp file for logging %s", err)
-		}
-		fname = f.Name()
-	}
-	logger.Hooks.Add(lfshook.NewHook(fname, lfmt))
-	registry.SetValue(registry.LOG_FILE_KEY, fname)
-	return logrus.NewEntry(logger), nil
+	return lfmt, nil
 }
