c5566ddaeb9f670793a37b8708933c24bb5e69e1
Siddhartha Basu <biosidd@gmail.com>
2020-01-13 14:07:18 -0600

    feat: add findOrCreateAnnoWithRank method

--- internal/load/stockcenter/stockcenter.go
+++ internal/load/stockcenter/stockcenter.go
@@ -89,6 +89,48 @@ func createAnno(client pb.TaggedAnnotationServiceClient, tag, id, ontology, valu
 	return nil
 }
 
+func findOrCreateAnnoWithRank(client pb.TaggedAnnotationServiceClient, tag, id, ontology, value string, rank int) (*pb.TaggedAnnotation, bool, error) {
+	create := false
+	ta, err := client.GetEntryAnnotation(
+		context.Background(),
+		&pb.EntryAnnotationRequest{
+			Tag:      tag,
+			EntryId:  id,
+			Ontology: ontology,
+			Rank:     int64(rank),
+		})
+	switch {
+	case err == nil:
+		return ta, create, nil
+	case status.Code(err) == codes.NotFound:
+		_, err := client.CreateAnnotation(
+			context.Background(),
+			&pb.NewTaggedAnnotation{
+				Data: &pb.NewTaggedAnnotation_Data{
+					Attributes: &pb.NewTaggedAnnotationAttributes{
+						Value:     value,
+						CreatedBy: regs.DEFAULT_USER,
+						Tag:       tag,
+						EntryId:   id,
+						Ontology:  ontology,
+						Rank:      int64(rank),
+					},
+				},
+			},
+		)
+		if err != nil {
+			return ta, create, fmt.Errorf(
+				"error in finding annotation %s for id %s %s",
+				tag,
+				id,
+				err,
+			)
+		}
+		create = true
+	}
+	return ta, create, nil
+}
+
 func findOrCreateAnnoWithStatus(client pb.TaggedAnnotationServiceClient, tag, id, ontology, value string) (bool, error) {
 	create := false
 	_, err := client.GetEntryAnnotation(
@@ -183,7 +225,7 @@ func delAnnotationGroup(client pb.TaggedAnnotationServiceClient, gc *pb.TaggedAn
 			&pb.GroupEntryId{GroupId: gcd.Group.GroupId},
 		)
 		if err != nil {
-			return err
+			return fmt.Errorf("error in deleting annotation group %s %s", gcd.Group.GroupId, err)
 		}
 		// remove all annotations
 		for _, gd := range gcd.Group.Data {
@@ -192,7 +234,7 @@ func delAnnotationGroup(client pb.TaggedAnnotationServiceClient, gc *pb.TaggedAn
 				&pb.DeleteAnnotationRequest{Id: gd.Id, Purge: true},
 			)
 			if err != nil {
-				return err
+				return fmt.Errorf("error in deleting annotation %s %s", gd.Id, err)
 			}
 		}
 	}
