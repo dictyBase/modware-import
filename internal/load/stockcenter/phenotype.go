8405433ab52e8eeb29765b4f9f322711b1a4d128
Siddhartha Basu <biosidd@gmail.com>
2020-01-13 13:13:26 -0600

    refactor: add debug level logging

--- internal/load/stockcenter/phenotype.go
+++ internal/load/stockcenter/phenotype.go
@@ -15,94 +15,123 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const (
-	phenoOntology = "Dicty Phenotypes"
-	envOntology   = "Dicty Environment"
-	assayOntology = "Dictyostellium Assay"
-	literatureTag = "literature_tag"
-	noteTag       = "public note"
-)
-
 func LoadPheno(cmd *cobra.Command, args []string) error {
 	pr := stockcenter.NewPhenotypeReader(registry.GetReader(regs.PHENO_READER))
 	client := regs.GetAnnotationAPIClient()
 	logger := registry.GetLogger()
-	phcount := 0
-	for pr.Next() {
-		pheno, err := pr.Value()
-		if err != nil {
-			return fmt.Errorf(
-				"error in loading phenotype for strain %s",
-				err,
-			)
-		}
-		gc, err := client.ListAnnotationGroups(
-			context.Background(),
-			&pb.ListGroupParameters{
-				Filter: fmt.Sprintf(
-					"entry_id==%s;tag==%s;ontology==%s",
-					pheno.StrainId,
-					pheno.Observation,
-					phenoOntology,
-				),
-			})
+	phenoMap, err := cachePhenotype(pr, logger)
+	if err != nil {
+		return err
+	}
+	count := 0
+	for id, phenoSlice := range phenoMap {
+		found := true
+		gc, err := getPhenotype(client, phenoSlice[0])
 		if err != nil {
 			if status.Code(err) != codes.NotFound { // error in lookup
+				return fmt.Errorf("error in getting phenotype of %s %s", id, err)
+			}
+			found = false
+			logger.WithFields(
+				logrus.Fields{
+					"type":  "phenotype",
+					"stock": "strain",
+					"event": "get",
+					"id":    id,
+				}).Debugf("no phenotype")
+		}
+		if found {
+			logger.WithFields(
+				logrus.Fields{
+					"type":  "phenotype",
+					"stock": "strain",
+					"event": "get",
+					"id":    id,
+				}).Debugf("retrieved phenotype")
+			if err := delAnnotationGroup(client, gc); err != nil {
 				return err
 			}
-			// no phenotype group found, create or find all individual annotations and phenotype
-			if err := handlePhenotype(client, pheno); err != nil {
-				return err
-			}
-			logger.Debugf("created phenotypes for %s strain", pheno.StrainId)
-			phcount++
-			continue
+			logger.WithFields(
+				logrus.Fields{
+					"type":  "phenotype",
+					"stock": "strain",
+					"event": "delete",
+					"id":    id,
+				}).Debugf("deleted phenotype")
 		}
-		if len(gc.Data) > 1 {
-			return fmt.Errorf(
-				"data constraint issue, got multiple phenotype groups for %s %s %s",
-				pheno.StrainId,
-				pheno.Observation,
-				phenoOntology,
-			)
-		}
-		// delete phenotype group
-		_, err = client.DeleteAnnotationGroup(
-			context.Background(),
-			&pb.GroupEntryId{GroupId: gc.Data[0].Group.GroupId},
-		)
+		err = createPhenotype(&strainPhenoArgs{
+			id:         id,
+			client:     client,
+			phenoSlice: phenoSlice,
+		})
 		if err != nil {
 			return err
 		}
-		//create or find all individual annotations and phenotype
-		if err := handlePhenotype(client, pheno); err != nil {
-			return err
-		}
-		logger.Debugf("flush and loaded phenotypes for %s strain", pheno.StrainId)
-		phcount++
+		logger.WithFields(
+			logrus.Fields{
+				"type":  "phenotype",
+				"stock": "strain",
+				"event": "create",
+				"id":    id,
+				"count": len(phenoSlice),
+			}).Debugf("created phenotypes")
+		count += len(phenoSlice)
 	}
-	logger.Infof("created %d phenotypes", phcount)
+	logger.WithFields(
+		logrus.Fields{
+			"type":  "phenotype",
+			"stock": "strains",
+			"event": "load",
+			"count": count,
+		}).Infof("loaded phenotypes")
 	return nil
 }
 
-func handlePhenotype(client pb.TaggedAnnotationServiceClient, ph *stockcenter.Phenotype) error {
-	var ids []string
-	phenMap := map[string][]string{
-		ph.Observation: []string{phenoOntology, "novalue"},
-		ph.Assay:       []string{assayOntology, "novalue"},
-		ph.Environment: []string{envOntology, "novalue"},
-		literatureTag:  []string{regs.DICTY_ANNO_ONTOLOGY, ph.LiteratureId},
-		noteTag:        []string{regs.DICTY_ANNO_ONTOLOGY, ph.Note},
-	}
-	for t, s := range phenMap {
-		t, err := findOrCreateAnno(client, t, ph.StrainId, s[0], s[1])
+func createPhenotype(args *strainPhenoArgs) error {
+	for i, pheno := range args.phenoSlice {
+		var ids []string
+		m := map[string][]string{
+			regs.PhenoOntology: []string{pheno.Observation, regs.EmptyValue},
+			regs.AssayOntology: []string{pheno.Assay, regs.EmptyValue},
+			regs.EnvOntology:   []string{pheno.Environment, regs.EmptyValue},
+			regs.DICTY_ANNO_ONTOLOGY: []string{
+				regs.LiteratureTag, pheno.LiteratureId,
+				regs.NoteTag, pheno.Note},
+		}
+	INNER:
+		for onto, dataSlice := range m {
+			if onto == regs.DICTY_ANNO_ONTOLOGY {
+				if len(dataSlice[1]) > 0 {
+					anno, err := createAnnoWithRank(args.client, dataSlice[0], args.id, onto, dataSlice[1], i)
+					if err != nil {
+						return err
+					}
+					ids = append(ids, anno.Data.Id)
+				}
+				if len(dataSlice[3]) > 0 {
+					anno, err := createAnnoWithRank(args.client, dataSlice[2], args.id, onto, dataSlice[3], i)
+					if err != nil {
+						return err
+					}
+					ids = append(ids, anno.Data.Id)
+				}
+				continue INNER
+			}
+			if len(dataSlice[1]) == 0 {
+				continue INNER
+			}
+			anno, err := createAnnoWithRank(args.client, dataSlice[0], args.id, onto, dataSlice[1], i)
+			if err != nil {
+				return err
+			}
+			ids = append(ids, anno.Data.Id)
+		}
+		_, err := args.client.CreateAnnotationGroup(context.Background(), &pb.AnnotationIdList{Ids: ids})
 		if err != nil {
 			return err
 		}
-		ids = append(ids, t.Data.Id)
 	}
-	_, err := client.CreateAnnotationGroup(context.Background(), &pb.AnnotationIdList{Ids: ids})
-	return err
+	return nil
 }
 
 func cachePhenotype(pr stockcenter.PhenotypeReader, logger *logrus.Entry) (map[string][]*stockcenter.Phenotype, error) {
@@ -130,4 +159,17 @@ func cachePhenotype(pr stockcenter.PhenotypeReader, logger *logrus.Entry) (map[s
 			"count": readCount,
 		}).Debugf("read all record")
 	return phenoMap, nil
+}
+
+func getPhenotype(client pb.TaggedAnnotationServiceClient, pheno *stockcenter.Phenotype) (*pb.TaggedAnnotationGroupCollection, error) {
+	return client.ListAnnotationGroups(
+		context.Background(),
+		&pb.ListGroupParameters{
+			Filter: fmt.Sprintf(
+				"entry_id==%s;tag==%s;ontology==%s",
+				pheno.StrainId,
+				pheno.Observation,
+				regs.PhenoOntology,
+			),
+		})
 }
