351777be2d76e3918af95f0c4f0cc49c1f1e9bb4
Siddhartha Basu <biosidd@gmail.com>
2020-01-14 15:27:31 -0600

    feat: add a separate file to add all constants for phenotype

---
+++ internal/registry/stockcenter/phenotype.go
@@ -0,0 +1,10 @@
+package stockcenter
+
+const (
+	PhenoOntology = "Dicty Phenotypes"
+	EnvOntology   = "Dicty Environment"
+	AssayOntology = "Dictyostelium Assay"
+	LiteratureTag = "pubmed id"
+	NoteTag       = "public note"
+	EmptyValue    = "novalue"
+)
