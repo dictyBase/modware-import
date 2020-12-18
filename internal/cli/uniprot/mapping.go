a73bc79bcb3075956809b733d0c90e54eebe864e
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-18 16:07:39 -0600

    feat: add cli commands for uniprot

---
+++ internal/cli/uniprot/mapping.go
@@ -0,0 +1,30 @@
+package uniprot
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// UniprotMappingCmd is for loading mapping of Uniprot IDs to Gene IDs.
+var UniprotMappingCmd = &cobra.Command{
+	Use:   "mapping",
+	Short: "load uniprot id mappings",
+	Args:  cobra.NoArgs,
+	// RunE:  loader.LoadUniprotMappings,
+	// PreRunE: func(cmd *cobra.Command, args []string) error {
+	// 	if err := setRedisClient(); err != nil {
+	// 		return err
+	// 	}
+	// 	return nil
+	// },
+}
+
+func init() {
+	UniprotMappingCmd.Flags().StringP(
+		"uniprot-url",
+		"u",
+		"https://www.uniprot.org/uniprot/?query=taxonomy:44689&columns=id,database(dictyBase),genes(PREFERRED)&format=tab",
+		"uniprot endpoint",
+	)
+	viper.BindPFlags(UniprotMappingCmd.Flags())
+}
