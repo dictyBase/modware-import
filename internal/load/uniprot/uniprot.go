ff1196203e0f7f66be9fe22ba8972335df18b2ea
wildlifehexagon <crossthebreeze@gmail.com>
2020-12-21 16:09:13 -0600

    refactor: move line reading into separate function

--- internal/load/uniprot/uniprot.go
+++ internal/load/uniprot/uniprot.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/dictyBase/modware-import/internal/registry"
+	r "github.com/go-redis/redis/v7"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -16,6 +17,15 @@ const (
 	// IDCacheKey is the key for storing redis hash field value
 	IDCacheKey = "UNIPROT2NAME/uniprot"
 )
+
+// Count represents the number of each item in the dataset
+type Count struct {
+	noMap      int
+	geneName   int
+	geneID     int
+	unresolved int
+	isoform    int
+}
 
 // LoadUniprotMappings stores uniprot and gene name or identifier mapping in redis
 func LoadUniprotMappings(cmd *cobra.Command, args []string) error {
@@ -27,62 +37,71 @@ func LoadUniprotMappings(cmd *cobra.Command, args []string) error {
 	}
 	defer resp.Body.Close()
 	scanner := bufio.NewScanner(resp.Body)
-	nc := 0
-	gnc := 0
-	gic := 0
-	urc := 0
-	sc := 0
+	c := &Count{
+		noMap:      0,
+		geneName:   0,
+		geneID:     0,
+		unresolved: 0,
+		isoform:    0,
+	}
 	for scanner.Scan() {
 		// ignore header
 		if strings.HasPrefix(scanner.Text(), "Entry") {
 			continue
 		}
 		s := strings.Split(strings.TrimSpace(scanner.Text()), "\t")
-		sl := len(s)
-		switch {
-		// if there is no mapping
-		case sl == 1:
-			nc++
-		// only gene ids
-		case sl == 2:
-			gic++
-			gs := strings.Split(s[1], ";")
-			if len(gs) > 3 {
-				log.Printf("unresolved line %s\t%s\n", s[0], s[1])
-				urc++
-			} else {
-				// store in redis
-				err := client.HSet(IDCacheKey, s[0], gs[0]).Err()
-				if err != nil {
-					return fmt.Errorf("error in setting the value in redis %s %s", s, err)
-				}
-			}
-		// gene name
-		case sl == 3:
-			gnc++
-			if strings.Contains(s[2], ";") {
-				sc++
-				ns := strings.Split(s[2], ";")
-				// store in redis
-				err := client.HSet(IDCacheKey, s[0], ns[0]).Err()
-				if err != nil {
-					return fmt.Errorf("error in setting the value in redis %s %s", s, err)
-				}
-			} else {
-				// store in redis
-				err := client.HSet(IDCacheKey, s[0], s[2]).Err()
-				if err != nil {
-					return fmt.Errorf("error in setting the value in redis %s %s", s, err)
-				}
-			}
-		default:
-			log.Printf("something seriously wrong with this line %s\n", s)
+		if err := readLine(s, c, client); err != nil {
+			return fmt.Errorf("error in scanning line %s", err)
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		return fmt.Errorf("error in scanning output %s", err)
 	}
-	stat := fmt.Sprintf("name:%d\tid:%d\tisoform:%d\tunresolved:%d\tnomap:%d\n", gnc, gic, sc, urc, nc)
+	stat := fmt.Sprintf("name:%d\tid:%d\tisoform:%d\tunresolved:%d\tnomap:%d\n", c.geneName, c.geneID, c.isoform, c.unresolved, c.noMap)
 	log.Print(stat)
+	return nil
+}
+
+func readLine(s []string, c *Count, client *r.Client) error {
+	sl := len(s)
+	switch {
+	// if there is no mapping
+	case sl == 1:
+		c.noMap++
+	// only gene ids
+	case sl == 2:
+		c.geneID++
+		gs := strings.Split(s[1], ";")
+		if len(gs) > 3 {
+			log.Printf("unresolved line %s\t%s\n", s[0], s[1])
+			c.unresolved++
+		} else {
+			// store in redis
+			err := client.HSet(IDCacheKey, s[0], gs[0]).Err()
+			if err != nil {
+				return fmt.Errorf("error in setting the value in redis %s %s", s, err)
+			}
+		}
+	// gene name
+	case sl == 3:
+		c.geneName++
+		if strings.Contains(s[2], ";") {
+			c.isoform++
+			ns := strings.Split(s[2], ";")
+			// store in redis
+			err := client.HSet(IDCacheKey, s[0], ns[0]).Err()
+			if err != nil {
+				return fmt.Errorf("error in setting the value in redis %s %s", s, err)
+			}
+		} else {
+			// store in redis
+			err := client.HSet(IDCacheKey, s[0], s[2]).Err()
+			if err != nil {
+				return fmt.Errorf("error in setting the value in redis %s %s", s, err)
+			}
+		}
+	default:
+		log.Printf("something seriously wrong with this line %s\n", s)
+	}
 	return nil
 }
