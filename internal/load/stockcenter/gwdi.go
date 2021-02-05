c941e5e4a1ab189334009d61e5fb58fcc42f9cdd
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-02-05 16:14:51 -0600

    refactor: group all strain creation code into a single method receiver structure

--- internal/load/stockcenter/gwdi.go
+++ internal/load/stockcenter/gwdi.go
@@ -18,180 +18,6 @@ import (
 	"google.golang.org/grpc/codes"
 )
 
-type gwdiCreate struct {
-	aclient        annotation.TaggedAnnotationServiceClient
-	sclient        pb.StockServiceClient
-	logger         *logrus.Entry
-	ctx            context.Context
-	user           string
-	value          string
-	genoTag        string
-	annoOntology   string
-	strainCharOnto string
-}
-
-func (gc *gwdiCreate) Execute(gwdi *stockcenter.GWDIStrain) error {
-	strain, err := createGwdi(gc.sclient, gwdi)
-	if err != nil {
-		return fmt.Errorf("error in creating new gwdi strain record  %s", err)
-	}
-	err = createAnno(&createAnnoArgs{
-		user:     gc.user,
-		id:       strain.Data.Id,
-		client:   gc.aclient,
-		ontology: gc.annoOntology,
-		tag:      gc.genoTag,
-		value:    gwdi.Genotype,
-	})
-	if err != nil {
-		return fmt.Errorf("cannot create genotype of gwdi strain %s %s", strain.Data.Id, err)
-	}
-	if err := gc.createPropAndChar(strain.Data.Id, gwdi); err != nil {
-		return err
-	}
-	gc.logger.WithFields(logrus.Fields{
-		"event": "create",
-		"id":    strain.Data.Id}).Debug("new gwdi strain record")
-	return nil
-}
-
-func (gc *gwdiCreate) createPropAndChar(id string, gwdi *stockcenter.GWDIStrain) error {
-	for _, char := range gwdi.Characters {
-		err := createAnno(&createAnnoArgs{
-			user:     gc.user,
-			id:       id,
-			client:   gc.aclient,
-			ontology: gc.strainCharOnto,
-			tag:      char,
-			value:    gc.value,
-		})
-		if err != nil {
-			return fmt.Errorf("cannot create characteristic %s of gwdi strain %s %s",
-				char, id, err,
-			)
-		}
-	}
-	for onto, prop := range gwdi.Properties {
-		err := createAnno(&createAnnoArgs{
-			user:     gc.user,
-			id:       id,
-			client:   gc.aclient,
-			ontology: onto,
-			tag:      prop.Property,
-			value:    prop.Value,
-		})
-		if err != nil {
-			return fmt.Errorf("cannot create property %s of gwdi strain %s %s",
-				prop.Property, id, err,
-			)
-		}
-	}
-	return nil
-}
-
-func createGwdi(client pb.StockServiceClient, gwdi *stockcenter.GWDIStrain) (*pb.Strain, error) {
-	attr := &pb.NewStrainAttributes{
-		CreatedBy:    regs.DEFAULT_USER,
-		UpdatedBy:    regs.DEFAULT_USER,
-		Summary:      gwdi.Summary,
-		Genes:        gwdi.Genes,
-		Depositor:    gwdi.Depositor,
-		Label:        gwdi.Label,
-		Species:      gwdi.Species,
-		Plasmid:      gwdi.Plasmid,
-		Parent:       gwdi.Parent,
-		Publications: []string{gwdi.Publication},
-		Names:        []string{gwdi.Name},
-	}
-	strain, err := client.CreateStrain(
-		context.Background(),
-		&pb.NewStrain{
-			Data: &pb.NewStrain_Data{
-				Type:       "strain",
-				Attributes: attr,
-			},
-		},
-	)
-	if err != nil {
-		return strain, fmt.Errorf("error in creating gwdi strain %s", err)
-	}
-	return strain, nil
-}
-
-func createConsumer(args *gwdiCreateConsumerArgs) chan error {
-	errc := make(chan error, 1)
-	counter := make(chan int, 1)
-	wg := new(sync.WaitGroup)
-	wg.Add(args.concurrency)
-	for i := 0; i < args.concurrency; i++ {
-		go func(args *gwdiCreateConsumerArgs) {
-			lc := 0
-			defer func() { counter <- lc }()
-			defer wg.Done()
-			for {
-				select {
-				case <-args.ctx.Done():
-					return
-				case gwdi, ok := <-args.tasks:
-					if !ok {
-						return
-					}
-					if err := args.runner.Execute(gwdi); err != nil {
-						errc <- err
-						args.cancelFn()
-						return
-					}
-				}
-				lc++
-			}
-		}(args)
-	}
-	go loadingCount(args.runner.logger, counter)
-	go syncLoader(wg, counter, errc)
-	return errc
-}
-
-func loadingCount(logger *logrus.Entry, counter chan int) {
-	c := 0
-	for v := range counter {
-		c = c + v
-	}
-	logger.WithFields(logrus.Fields{
-		"type":  "counter",
-		"count": c,
-	}).Infof("loaded gwdi strains")
-}
-
-func syncLoader(wg *sync.WaitGroup, counter chan int, errc chan error) {
-	wg.Wait()
-	close(counter)
-	close(errc)
-}
-
-func createProducer(args *gwdiCreateProdArgs) (chan *stockcenter.GWDIStrain, chan error) {
-	tasks := make(chan *stockcenter.GWDIStrain)
-	errc := make(chan error, 1)
-	go func(args *gwdiCreateProdArgs) {
-		defer close(tasks)
-		defer close(errc)
-		for args.gr.Next() {
-			gwdi, err := args.gr.Value()
-			select {
-			case <-args.ctx.Done():
-				return
-			default:
-				if err != nil {
-					errc <- err
-					args.cancelFn()
-					return
-				}
-				tasks <- gwdi
-			}
-		}
-	}(args)
-	return tasks, errc
-}
-
 func LoadGwdi(cmd *cobra.Command, args []string) error {
 	logger := registry.GetLogger().WithFields(logrus.Fields{
 		"type":  "gwdi",
@@ -264,6 +90,80 @@ func runConcurrentCreate(logger *logrus.Entry, gr stockcenter.GWDIMutantReader) 
 	})
 	errcList = append(errcList, errc)
 	return waitForPipeline(errcList...)
+}
+
+func createConsumer(args *gwdiCreateConsumerArgs) chan error {
+	errc := make(chan error, 1)
+	counter := make(chan int, 1)
+	wg := new(sync.WaitGroup)
+	wg.Add(args.concurrency)
+	for i := 0; i < args.concurrency; i++ {
+		go func(args *gwdiCreateConsumerArgs) {
+			lc := 0
+			defer func() { counter <- lc }()
+			defer wg.Done()
+			for {
+				select {
+				case <-args.ctx.Done():
+					return
+				case gwdi, ok := <-args.tasks:
+					if !ok {
+						return
+					}
+					if err := args.runner.execute(gwdi); err != nil {
+						errc <- err
+						args.cancelFn()
+						return
+					}
+				}
+				lc++
+			}
+		}(args)
+	}
+	go loadingCount(args.runner.logger, counter)
+	go syncLoader(wg, counter, errc)
+	return errc
+}
+
+func loadingCount(logger *logrus.Entry, counter chan int) {
+	c := 0
+	for v := range counter {
+		c = c + v
+	}
+	logger.WithFields(logrus.Fields{
+		"type":  "counter",
+		"count": c,
+	}).Infof("loaded gwdi strains")
+}
+
+func syncLoader(wg *sync.WaitGroup, counter chan int, errc chan error) {
+	wg.Wait()
+	close(counter)
+	close(errc)
+}
+
+func createProducer(args *gwdiCreateProdArgs) (chan *stockcenter.GWDIStrain, chan error) {
+	tasks := make(chan *stockcenter.GWDIStrain)
+	errc := make(chan error, 1)
+	go func(args *gwdiCreateProdArgs) {
+		defer close(tasks)
+		defer close(errc)
+		for args.gr.Next() {
+			gwdi, err := args.gr.Value()
+			select {
+			case <-args.ctx.Done():
+				return
+			default:
+				if err != nil {
+					errc <- err
+					args.cancelFn()
+					return
+				}
+				tasks <- gwdi
+			}
+		}
+	}(args)
+	return tasks, errc
 }
 
 type gwdiDel struct {
@@ -380,4 +280,104 @@ func (gd *gwdiDel) execute(id string) error {
 		"count": len(tac.Data),
 	}).Debug("remove gwdi strain annotations")
 	return nil
+}
+
+type gwdiCreate struct {
+	aclient        annotation.TaggedAnnotationServiceClient
+	sclient        pb.StockServiceClient
+	logger         *logrus.Entry
+	ctx            context.Context
+	user           string
+	value          string
+	genoTag        string
+	annoOntology   string
+	strainCharOnto string
+}
+
+func (gc *gwdiCreate) execute(gwdi *stockcenter.GWDIStrain) error {
+	strain, err := gc.createGwdi(gwdi)
+	if err != nil {
+		return fmt.Errorf("error in creating new gwdi strain record  %s", err)
+	}
+	err = createAnno(&createAnnoArgs{
+		user:     gc.user,
+		id:       strain.Data.Id,
+		client:   gc.aclient,
+		ontology: gc.annoOntology,
+		tag:      gc.genoTag,
+		value:    gwdi.Genotype,
+	})
+	if err != nil {
+		return fmt.Errorf("cannot create genotype of gwdi strain %s %s", strain.Data.Id, err)
+	}
+	if err := gc.createPropAndChar(strain.Data.Id, gwdi); err != nil {
+		return err
+	}
+	gc.logger.WithFields(logrus.Fields{
+		"event": "create",
+		"id":    strain.Data.Id}).Debug("new gwdi strain record")
+	return nil
+}
+
+func (gc *gwdiCreate) createPropAndChar(id string, gwdi *stockcenter.GWDIStrain) error {
+	for _, char := range gwdi.Characters {
+		err := createAnno(&createAnnoArgs{
+			user:     gc.user,
+			id:       id,
+			client:   gc.aclient,
+			ontology: gc.strainCharOnto,
+			tag:      char,
+			value:    gc.value,
+		})
+		if err != nil {
+			return fmt.Errorf("cannot create characteristic %s of gwdi strain %s %s",
+				char, id, err,
+			)
+		}
+	}
+	for onto, prop := range gwdi.Properties {
+		err := createAnno(&createAnnoArgs{
+			user:     gc.user,
+			id:       id,
+			client:   gc.aclient,
+			ontology: onto,
+			tag:      prop.Property,
+			value:    prop.Value,
+		})
+		if err != nil {
+			return fmt.Errorf("cannot create property %s of gwdi strain %s %s",
+				prop.Property, id, err,
+			)
+		}
+	}
+	return nil
+}
+
+func (gc *gwdiCreate) createGwdi(gwdi *stockcenter.GWDIStrain) (*pb.Strain, error) {
+	attr := &pb.NewStrainAttributes{
+		CreatedBy:    regs.DEFAULT_USER,
+		UpdatedBy:    regs.DEFAULT_USER,
+		Summary:      gwdi.Summary,
+		Genes:        gwdi.Genes,
+		Depositor:    gwdi.Depositor,
+		Label:        gwdi.Label,
+		Species:      gwdi.Species,
+		Plasmid:      gwdi.Plasmid,
+		Parent:       gwdi.Parent,
+		Publications: []string{gwdi.Publication},
+		Names:        []string{gwdi.Name},
+	}
+	strain, err := gc.sclient.CreateStrain(
+		context.Background(),
+		&pb.NewStrain{
+			Data: &pb.NewStrain_Data{
+				Type:       "strain",
+				Attributes: attr,
+			},
+		},
+	)
+	if err != nil {
+		return strain, fmt.Errorf("error in creating gwdi strain %s", err)
+	}
+	return strain, nil
 }
