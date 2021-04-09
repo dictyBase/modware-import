57352744fe8d408daca9573e762b5c760365fd2f
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-04-09 17:24:31 -0500

    feat: add strain property ontology and gwdi tag in the registry

--- internal/registry/stockcenter/inventory.go
+++ internal/registry/stockcenter/inventory.go
@@ -3,6 +3,7 @@ package stockcenter
 const (
 	InvReader         = "inv-input"
 	StrainInvOnto     = "strain_inventory"
+	StrainCharOnto    = "dicty_strain_property"
 	PlasmidInvOntO    = "plasmid_inventory"
 	InvExistValue     = "value"
 	InvLocationTag    = "location"
@@ -15,4 +16,5 @@ const (
 	InvObtainedAsTag  = "clear"
 	PlasmidInvTag     = "plasmid inventory"
 	StrainInvTag      = "strain_inventory"
+	GWDIStrainTag     = "REMI-seq"
 )
