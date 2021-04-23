9df460fafbb483a820144364368d9be6f96a3d4a
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-04-23 16:14:27 -0500

    refactor: move and compact redundant code to runner and env packages

--- internal/runner/annotation/annotation.go
+++ internal/runner/annotation/annotation.go
@@ -3,31 +3,13 @@ package annotation
 import (
 	"github.com/dictyBase/modware-import/internal/runner"
 	"github.com/magefile/mage/mg"
-	"github.com/magefile/mage/sh"
 )
-
-// Build builds the binary for modware-import project
-func Build() error {
-	if err := runner.BuildSetup(); err != nil {
-		return err
-	}
-	return sh.Run("go", "build", "-o", "importer", "cmd/import/main.go")
-}
 
 // Clean deletes all order data from arangodb database
 func Clean() error {
-	if err := runner.ArangoEnvs(); err != nil {
-		return err
-	}
-	mg.Deps(Build)
-	return sh.Run(
-		"./importer",
-		"--log-level",
-		"info",
-		"--is-secure",
-		"arangodb",
-		"delete",
-		"-d",
-		"annotation",
+	mg.SerialDeps(
+		runner.Build,
+		mg.F(runner.CleanDb, "annotation"),
 	)
+	return nil
 }
--- magefile.go
+++ magefile.go
@@ -4,9 +4,8 @@ package main
 
 import (
 	"github.com/magefile/mage/mg"
-	"github.com/magefile/mage/sh"
-
-	"github.com/dictyBase/modware-import/internal/runner"
+	// mage:import
+	_ "github.com/dictyBase/modware-import/internal/runner"
 	// mage:import stock
 	"github.com/dictyBase/modware-import/internal/runner/stock"
 	// mage:import annotation
@@ -14,16 +13,6 @@ import (
 	// mage:import order
 	"github.com/dictyBase/modware-import/internal/runner/order"
 )
-
-const ()
-
-// Build builds the binary for modware-import project
-func Build() error {
-	if err := runner.BuildSetup(); err != nil {
-		return err
-	}
-	return sh.Run("go", "build", "-o", "importer", "cmd/import/main.go")
-}
 
 // CleanAll deletes all data from stock,order and annotation databases
 func CleanAll() error {
