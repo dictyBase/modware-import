0e8d9718002a9d128c5007f72e5dbfaa41bead2d
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-04-27 17:31:30 -0500

    feat: add command for loading obojson formatted ontology files

---
+++ internal/cli/ontology/load.go
@@ -0,0 +1,47 @@
+package ontology
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// LoadCmd obojson formmated ontologies to arangodb
+var LoadCmd = &cobra.Command{
+	Use:   "load",
+	Short: "load an obojson formatted ontologies to arangodb",
+	Args:  cobra.NoArgs,
+}
+
+func init() {
+	loadFlags()
+	viper.BindPFlags(LoadCmd.Flags())
+}
+
+func loadFlags() {
+	LoadCmd.Flags().StringSliceP(
+		"obojson",
+		"j",
+		[]string{""},
+		"input ontology files in obograph json format",
+	)
+	LoadCmd.Flags().String(
+		"obograph",
+		"obograph",
+		"arangodb named graph for managing ontology graph",
+	)
+	LoadCmd.Flags().String(
+		"cv-collection",
+		"cv",
+		"arangodb collection for storing ontology information",
+	)
+	LoadCmd.Flags().String(
+		"rel-collection",
+		"cvterm_relationship",
+		"arangodb collection for storing cvterm relationships",
+	)
+	LoadCmd.Flags().String(
+		"term-collection",
+		"cvterm",
+		"arangodb collection for storing ontoloy terms",
+	)
+}
