66dbe70f957ad7dab78f811c184b6ca518909659
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-04-28 11:05:59 -0500

    refactor: lift up the s3 bucket flags to ontology subcommand level

---
+++ internal/cli/ontology/refresh.go
@@ -0,0 +1,103 @@
+package ontology
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/dictyBase/modware-import/internal/registry"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/minio/minio-go/v6"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// RefreshCmd updates obojson formatted ontologies in S3(minio) storage
+var RefreshCmd = &cobra.Command{
+	Use:   "refresh",
+	Short: "updates ontologies in S3 storage",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		dir, err := cloneRepo()
+		if err != nil {
+			return err
+		}
+		files, err := obojsonFiles(dir)
+		if err != nil {
+			return err
+		}
+		logger := registry.GetLogger()
+		for _, e := range files {
+			_, err := registry.GetS3Client().FPutObject(
+				viper.GetString("s3-bucket"),
+				viper.GetString("s3-bucket-path"),
+				e, minio.PutObjectOptions{},
+			)
+			if err != nil {
+				return err
+			}
+			logger.Infof("uploaded %s file", filepath.Base(e))
+		}
+		return nil
+	},
+}
+
+func init() {
+	refreshFlags()
+	viper.BindPFlags(RefreshCmd.Flags())
+}
+
+func refreshFlags() {
+	RefreshCmd.Flags().String(
+		"repository",
+		"https://github.com/dictybase-playground/ontomania",
+		"github repository source of obojson files",
+	)
+	RefreshCmd.Flags().String(
+		"branch",
+		"develop",
+		"branch of github repository",
+	)
+	RefreshCmd.Flags().String(
+		"subfolder",
+		"obograph-json",
+		"sub folder of clone repository for the source of obojson files",
+	)
+}
+
+func obojsonFiles(dir string) ([]string, error) {
+	fullPath := filepath.Join(dir, viper.GetString("subfolder"))
+	entries, err := os.ReadDir(fullPath)
+	if err != nil {
+		return []string{""}, err
+	}
+	var files []string
+	for _, e := range entries {
+		if e.IsDir() {
+			continue
+		}
+		if !strings.HasSuffix(e.Name(), "json") {
+			continue
+		}
+		files = append(files, filepath.Join(fullPath, e.Name()))
+	}
+	return files, nil
+}
+
+func cloneRepo() (string, error) {
+	dir, err := os.MkdirTemp(os.TempDir(), "*-github")
+	if err != nil {
+		return dir, fmt.Errorf("error in making temp folder %s", err)
+	}
+	_, err = git.PlainClone(dir, false, &git.CloneOptions{
+		URL:          viper.GetString("repository"),
+		SingleBranch: true,
+		ReferenceName: plumbing.NewBranchReferenceName(
+			viper.GetString("branch"),
+		)},
+	)
+	return dir, err
+}
