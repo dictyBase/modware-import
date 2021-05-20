f0a3e50d0730afc520d08a6c69860a0f4e59afe9
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-05-19 20:39:25 -0500

    refactor: remove git branch option from ontology target

--- internal/runner/ontology/ontology.go
+++ internal/runner/ontology/ontology.go
@@ -1,8 +1,6 @@
 package ontology
 
 import (
-	"fmt"
-
 	"github.com/dictyBase/modware-import/internal/runner"
 	"github.com/dictyBase/modware-import/internal/runner/env"
 	"github.com/magefile/mage/mg"
@@ -10,55 +8,45 @@ import (
 )
 
 // Refresh gets all obojson formatted files from source github repository
-// and stores in S3(minio) for later upload. The ontology group and git branch have to be supplied.
-func Refresh(group, branch string) error {
+// and stores in S3(minio) for later upload. The ontology group and binary path have to be supplied.
+func Refresh(bin, group string) error {
 	if err := env.MinioEnvs(); err != nil {
 		return err
 	}
-	mg.Deps(mg.F(runner.BuildBranch, branch))
 	s := runner.TermSpinner("Refreshing objson files ...")
 	defer s.Stop()
 	s.Start()
 	return sh.Run(
-		fmt.Sprintf("./%s", runner.Command),
-		"--log-level",
-		runner.LogLevel,
-		"--access-key",
-		env.MinioAccessKey(),
-		"--secret-key",
-		env.MinioSecretKey(),
-		"ontology",
-		"refresh",
-		"--group",
-		group,
+		bin, "--log-level", runner.LogLevel,
+		"--access-key", env.MinioAccessKey(),
+		"--secret-key", env.MinioSecretKey(),
+		"ontology", "refresh",
+		"--group", group,
 	)
 }
 
 // Load loads all obograph-json formatted ontologies
 // The ontology group and git branch have to be supplied.
-func Load(group, branch string) error {
+func Load(group string) error {
+	bin, err := runner.LookUp()
+	if err != nil {
+		return err
+	}
 	if err := env.ArangoEnvs(); err != nil {
 		return err
 	}
 	if err := env.ArangoDBName(); err != nil {
 		return err
 	}
-	mg.Deps(mg.F(Refresh, group, branch))
+	mg.Deps(mg.F(Refresh, bin, group))
 	s := runner.TermSpinner("loading obojson ontology files ...")
 	defer s.Stop()
 	s.Start()
 	return sh.Run(
-		fmt.Sprintf("./%s", runner.Command),
-		"ontology",
-		"--log-level",
-		runner.LogLevel,
-		"--access-key",
-		env.MinioAccessKey(),
-		"--secret-key",
-		env.MinioSecretKey(),
-		"--is-secure",
-		"load",
-		"--group",
-		group,
+		bin, "ontology", "--log-level", runner.LogLevel,
+		"--access-key", env.MinioAccessKey(),
+		"--secret-key", env.MinioSecretKey(),
+		"--is-secure", "load",
+		"--group", group,
 	)
 }
