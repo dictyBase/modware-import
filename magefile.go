cbfb05d020a8c30ffd70e23f47e53375803db924
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-06-24 18:21:23 -0500

    feat: add LoadData target which includes cleanup and loading the ontologies

--- magefile.go
+++ magefile.go
@@ -8,8 +8,12 @@ import (
 	"github.com/dictyBase/modware-import/internal/runner"
 	// mage:import stock
 	_ "github.com/dictyBase/modware-import/internal/runner/stock"
+	stock "github.com/dictyBase/modware-import/internal/runner/stock"
+
 	// mage:import ontology
 	_ "github.com/dictyBase/modware-import/internal/runner/ontology"
+	onto "github.com/dictyBase/modware-import/internal/runner/ontology"
+
 	// mage:import data
 	_ "github.com/dictyBase/modware-import/internal/runner/data"
 )
@@ -21,5 +25,15 @@ func CleanAllDB() error {
 	for _, db := range dbs {
 		mg.Deps(mg.F(runner.CleanDB, db))
 	}
+	return nil
+}
+
+// LoadData will clean the collections, load ontologies and then load all the data
+func LoadData() error {
+	mg.SerialDeps(
+		CleanAllDB,
+		mg.F(onto.Load, "obojson"),
+		stock.LoadStrain,
+	)
 	return nil
 }
