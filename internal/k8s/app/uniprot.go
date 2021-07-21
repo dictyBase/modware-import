833a0375ca87d1dbecbca5c6d3c3742e46899deb
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-07-21 18:15:36 -0500

    refactor: move few reusable methods to SimpleJobApp struct

--- internal/k8s/app/uniprot.go
+++ internal/k8s/app/uniprot.go
@@ -7,24 +7,31 @@ import (
 	"github.com/dictyBase/modware-import/internal/k8s"
 	batch "k8s.io/api/batch/v1"
 	apiv1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
 type UniprotLoader struct {
 	*k8s.SimpleJobApp
-	LogLevel string
 }
 
 func NewUniprotLoader(args *k8s.AppParams, ispec *k8s.ImageSpec, level string) (*UniprotLoader, error) {
-	app, err := k8s.NewApp(args, ispec)
+	app, err := k8s.NewSimpleJobApp(args, ispec, level)
 	if err != nil {
 		return &UniprotLoader{}, err
 	}
 	return &UniprotLoader{
 		SimpleJobApp: app,
-		LogLevel:     level,
 	}, err
+}
+
+func (u *UniprotLoader) Command() []string {
+	return []string{
+		"/usr/local/bin/importer",
+		"uniprot",
+		"mapping",
+	}
 }
 
 // Install will attempt to install in Kubernetes
@@ -55,34 +62,6 @@ func (u *UniprotLoader) Install(client *kubernetes.Clientset) error {
 	return nil
 }
 
-// Uninstall will attempt to uninstall in Kubernetes
-func (u *UniprotLoader) Uninstall(client *kubernetes.Clientset) error {
-	return client.BatchV1().
-		Jobs(u.Meta().Namespace).
-		Delete(
-			context.Background(),
-			u.Meta().Name,
-			metav1.DeleteOptions{},
-		)
-}
-
-func (u *UniprotLoader) Command() []string {
-	return []string{
-		"/usr/local/bin/importer",
-		"uniprot",
-		"mapping",
-	}
-}
-
-func (u *UniprotLoader) TemplatePodSpecMeta() metav1.ObjectMeta {
-	return metav1.ObjectMeta{
-		Name: u.Meta().Name,
-		Labels: map[string]string{
-			"app": u.Meta().Name,
-		},
-	}
-}
-
 func (u *UniprotLoader) TemplatePodSpec() (apiv1.PodSpec, error) {
 	contName, err := k8s.RandContainerName(u.Meta().Name, "import", 10)
 	if err != nil {
@@ -94,11 +73,15 @@ func (u *UniprotLoader) TemplatePodSpec() (apiv1.PodSpec, error) {
 				Name:    contName,
 				Image:   u.ImageManifest(),
 				Command: u.Command(),
-				Env: append(
-					k8s.MinioSecManifest(),
-					k8s.LogEnvManifest(u.LogLevel)...,
-				),
+				Env:     u.ContainerEnv(),
 			},
 		},
 	}, nil
+}
+
+func (u *UniprotLoader) ContainerEnv() []v1.EnvVar {
+	return append(
+		k8s.MinioSecManifest(),
+		k8s.LogEnvManifest(u.LogLevel())...,
+	)
 }
--- internal/k8s/simple_job.go
+++ internal/k8s/simple_job.go
@@ -1,22 +1,27 @@
 package k8s
 
 import (
+	"context"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 type SimpleJobApp struct {
 	metav1.ObjectMeta
 	*ImageSpec
 	description string
+	level       string
 	randomizer  map[string]func() []string
 }
 
-func NewApp(args *AppParams, ispec *ImageSpec) (*SimpleJobApp, error) {
+func NewApp(args *AppParams, ispec *ImageSpec, level string) (*SimpleJobApp, error) {
 	qname, err := RandomFullName(args.Name, args.fragment, 10)
 	if err != nil {
 		return &SimpleJobApp{}, err
 	}
 	return &SimpleJobApp{
+		level:       level,
 		description: args.Description,
 		ImageSpec:   ispec,
 		ObjectMeta: metav1.ObjectMeta{
@@ -38,4 +43,28 @@ func (a *SimpleJobApp) Meta() *metav1.ObjectMeta {
 // Description returns the application description
 func (a *SimpleJobApp) Description() string {
 	return a.description
+}
+
+func (a *SimpleJobApp) TemplatePodSpecMeta() metav1.ObjectMeta {
+	return metav1.ObjectMeta{
+		Name: a.Meta().Name,
+		Labels: map[string]string{
+			"app": a.Meta().Name,
+		},
+	}
+}
+
+// Uninstall will attempt to uninstall in Kubernetes
+func (a *SimpleJobApp) Uninstall(client *kubernetes.Clientset) error {
+	return client.BatchV1().
+		Jobs(a.Meta().Namespace).
+		Delete(
+			context.Background(),
+			a.Meta().Name,
+			metav1.DeleteOptions{},
+		)
+}
+
+func (a *SimpleJobApp) LogLevel() string {
+	return a.level
 }
