bdee2ec37cd75db4d9bf23fe7dfa703035a55d16
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-10-08 12:07:02 -0500

    fix: add regular expression to check for branch or tag name

--- internal/git/git.go
+++ internal/git/git.go
@@ -3,10 +3,13 @@ package git
 import (
 	"fmt"
 	"os"
+	"regexp"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 )
+
+var rgxp = regexp.MustCompile(`^v?([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$`)
 
 func CloneRepo(repo, branch string) (string, error) {
 	dir, err := os.MkdirTemp(os.TempDir(), "*-github")
@@ -14,9 +17,16 @@ func CloneRepo(repo, branch string) (string, error) {
 		return dir, fmt.Errorf("error in making temp folder %s", err)
 	}
 	_, err = git.PlainClone(dir, false, &git.CloneOptions{
-		ReferenceName: plumbing.NewBranchReferenceName(branch),
+		ReferenceName: gitRefName(branch),
 		URL:           repo,
 		SingleBranch:  true,
 	})
 	return dir, err
+}
+
+func gitRefName(ref string) plumbing.ReferenceName {
+	if rgxp.MatchString(ref) {
+		return plumbing.NewTagReferenceName(ref)
+	}
+	return plumbing.NewBranchReferenceName(ref)
 }
