af2d6623f2fbe00acda6a8bca49af77a85408bd3
Siddhartha Basu <biosidd@gmail.com>
2022-07-11 18:55:00 -0500

    feat: add the root cli for kubernetes deployment command

---
+++ internal/k8s/cli.go
@@ -0,0 +1,57 @@
+package k8s
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/cockroachdb/errors"
+	"github.com/dictyBase/modware-import/internal/cli"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "k8s",
+	Short: "cli for deploying and running import commands in kubernetes cluster",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		if err := cli.PersistentPreRun(cmd); err != nil {
+			return errors.Errorf("error in executing pre run %s", err)
+		}
+		return nil
+	},
+	PersistentPostRunE: func(cmd *cobra.Command, args []string) error {
+		if err := cli.PersistentPostRun(cmd); err != nil {
+			return errors.Errorf("error in executing post-run %s", err)
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := cli.RunDoc(cmd); err != nil {
+			return errors.Errorf("error in generating docs %s", err)
+		}
+		return nil
+	},
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	RootCmd.PersistentFlags().String(
+		"kubeconfig",
+		"",
+		"path to the kubernetes client(kubeconfig) file[REQUIRED]",
+	)
+	_ = RootCmd.MarkFlagRequired("kubeconfig")
+	cli.LoggingArgs(RootCmd)
+	cli.S3Args(RootCmd)
+	viper.BindPFlags(RootCmd.Flags())
+	viper.BindPFlags(RootCmd.PersistentFlags())
+}
