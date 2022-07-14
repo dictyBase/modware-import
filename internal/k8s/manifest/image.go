a44db4640e7203472f85de700b39ef1c65fc5a50
Siddhartha Basu <biosidd@gmail.com>
2022-07-13 22:14:43 -0500

    refactor: rename image name formatter function

--- internal/k8s/manifest/image.go
+++ internal/k8s/manifest/image.go
@@ -4,19 +4,6 @@ import (
 	"fmt"
 )
 
-type ImageSpec struct {
-	repo, tag  string
-	PullPolicy string
-}
-
-func NewImageSpec(repo, tag, policy string) *ImageSpec {
-	return &ImageSpec{
-		repo:       repo,
-		tag:        tag,
-		PullPolicy: policy,
-	}
-}
-
-func (s *ImageSpec) ImageManifest() string {
-	return fmt.Sprintf("%s:%s", s.repo, s.tag)
+func Image(repo, tag string) string {
+	return fmt.Sprintf("%s:%s", repo, tag)
 }
