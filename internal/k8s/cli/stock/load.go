c7bb57f940ebd34e1c27976a88a41f4cf5db8400
Siddhartha Basu <biosidd@gmail.com>
2022-07-20 17:03:21 -0500

    feat: add k8s subcommand for loading stock data

---
+++ internal/k8s/cli/stock/load.go
@@ -0,0 +1,45 @@
+package stock
+
+import (
+	cliJob "github.com/dictyBase/modware-import/internal/k8s/cli/job"
+	"github.com/dictyBase/modware-import/internal/registry"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var LoadStockCmd = &cobra.Command{
+	Use:   "load-ontology",
+	Short: "run stock load command as a kubernetes job in the cluster",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		labels := cliJob.MetaLabel()
+		labels["subcommand"] = "load-stock"
+		for _, target := range viper.GetStringSlice("targets") {
+			job, err := cliJob.Run(
+				cmd,
+				labels,
+				LoadCommand(target),
+			)
+			if err != nil {
+				return errors.Errorf("error in running job %s for target %s", err, target)
+			}
+			registry.GetLogger().Infof("deployed job %s for target %s", job.Name, target)
+		}
+
+		return nil
+	},
+}
+
+func LoadCommand(target string) []string {
+	return []string{
+		"/usr/local/bin/gmake",
+		target,
+	}
+}
+
+func init() {
+	LoadStockCmd.Flags().
+		StringArray("targets", []string{"stock:loadStrain"}, "targets for loading stock data")
+	viper.BindPFlags(LoadStockCmd.Flags())
+}
