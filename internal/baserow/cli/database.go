6ab0c60acaa15d9bb72f22e599e621ade2e8671d
Siddhartha Basu <biosidd@gmail.com>
2023-06-28 14:15:32 -0500

    feat(cli): add CreateDatabaseToken function to generate a token for accessing the database
    The CreateDatabaseToken function is added to generate a token for
    accessing the database. It uses the provided email and password to
    obtain a token from the server. The email flag is added to the
    CreateDatabaseTokenFlag function to allow the user to specify the email
    of the user for which the token should be generated.

--- internal/baserow/cli/database.go
+++ internal/baserow/cli/database.go
@@ -1,15 +1,47 @@
 package cli
 
 import (
+	"context"
+	"fmt"
+
+	"net/http"
+
+	"github.com/dictyBase/modware-import/internal/baserow/client"
 	"github.com/urfave/cli/v2"
 )
+
+func CreateDatabaseToken(c *cli.Context) error {
+	conf := client.NewConfiguration()
+	conf.Host = c.String("server")
+	conf.Scheme = "https"
+	bclient := client.NewAPIClient(conf)
+	req := bclient.UserApi.TokenAuth(context.Background())
+	resp, r, err := req.TokenObtainPairWithUser(
+		client.TokenObtainPairWithUser{
+			Email:    client.PtrString(c.String("email")),
+			Password: c.String("password"),
+		},
+	).Execute()
+	defer r.Body.Close()
+	if err != nil {
+		return cli.Exit(
+			fmt.Sprintf("error in executing API call %s", err),
+			2,
+		)
+	}
+	if r != nil && r.StatusCode == http.StatusUnauthorized {
+		return cli.Exit("unauthrorized access", 2)
+	}
+	fmt.Printf("access token %s\n", resp.GetAccessToken())
+	return nil
+}
 
 func CreateDatabaseTokenFlag() []cli.Flag {
 	return []cli.Flag{
 		&cli.StringFlag{
-			Name:     "user",
-			Aliases:  []string{"u"},
-			Usage:    "Database user",
+			Name:     "email",
+			Aliases:  []string{"e"},
+			Usage:    "Email of the user",
 			Required: true,
 		},
 		&cli.StringFlag{
