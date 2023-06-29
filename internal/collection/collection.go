1acb7b5683dbdc15ad93fc6800c6d36aafae3eb7
Siddhartha Basu <biosidd@gmail.com>
2023-06-29 05:39:21 -0500

    refactor(collection.go): replace custom Contains and Index functions with slices.Contains
    The custom Contains and Index functions in the collection package have
    been replaced with the slices.Contains function from the
    golang.org/x/exp/slices package.

--- internal/collection/collection.go
+++ internal/collection/collection.go
@@ -1,5 +1,9 @@
 package collection
 
+import (
+	"golang.org/x/exp/slices"
+)
+
 // Map returns the slice obtained after applying the given function over every
 // element in the given slice
 func Map[T1, T2 any](s []T1, fn func(T1) T2) []T2 {
@@ -14,33 +18,11 @@ func Map[T1, T2 any](s []T1, fn func(T1) T2) []T2 {
 func Remove(a []string, items ...string) []string {
 	var s []string
 	for _, v := range a {
-		if !Contains(items, v) {
+		if !slices.Contains(items, v) {
 			s = append(s, v)
 		}
 	}
 	return s
-}
-
-// Contains reports whether s is present in slice a
-func Contains(a []string, s string) bool {
-	if len(a) == 0 {
-		return false
-	}
-	return Index(a, s) >= 0
-}
-
-// Index returns the index of the first instance of s in slice a, or -1 if s is
-// not present in a
-func Index(a []string, s string) int {
-	if len(a) == 0 {
-		return -1
-	}
-	for i, v := range a {
-		if v == s {
-			return i
-		}
-	}
-	return -1
 }
 
 // Extend appends all the elements of slices to a new slice
