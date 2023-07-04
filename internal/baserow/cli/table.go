78c3904ebab935570513af7c52297270a0cdacb9
Siddhartha Basu <biosidd@gmail.com>
2023-07-04 16:30:20 -0500

    feat(cli): add LoadOntologyToTable command to load ontology fields into a table
    The LoadOntologyToTable function is added to the CLI, which loads
    ontology fields into a table. It retrieves the list of table fields and
    creates fields if they don't exist. The CreateTable function is fixed to
    create a table with the specified name. The LoadOntologyToTableFlag
    function is added to provide the necessary flags for the
    LoadOntologyToTable command.

--- internal/baserow/cli/table.go
+++ internal/baserow/cli/table.go
@@ -9,6 +9,70 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+func LoadOntologyToTable(c *cli.Context) error {
+	logger := registry.GetLogger()
+	bclient := baserowClient(c.String("server"))
+	authCtx := context.WithValue(
+		context.Background(),
+		client.ContextAccessToken,
+		c.String("token"),
+	)
+	tlist, resp, err := bclient.
+		DatabaseTableFieldsApi.
+		ListDatabaseTableFields(authCtx, int32(c.Int("table-id"))).
+		Execute()
+	if err != nil {
+		return cli.Exit(
+			fmt.Errorf("error in getting list of table fields %s", err), 2,
+		)
+	}
+	defer resp.Body.Close()
+	if len(tlist) == 0 {
+		logger.Debug("need to create fields in the table")
+		_, trsp, err := bclient.
+			DatabaseTableFieldsApi.
+			CreateDatabaseTableField(authCtx, int32(c.Int("table-id"))).
+			FieldCreateField(client.FieldCreateField{
+				BooleanFieldCreateField: client.NewBooleanFieldCreateField(
+					"Is_obsolete",
+					client.BOOLEAN,
+				),
+			}).Execute()
+		if err != nil {
+			return cli.Exit(
+				fmt.Errorf(
+					"error in creating table field Is_obsolete %s",
+					err,
+				),
+				2,
+			)
+		}
+		defer trsp.Body.Close()
+		for _, field := range []string{"Name", "Id"} {
+			_, frsp, err := bclient.
+				DatabaseTableFieldsApi.
+				CreateDatabaseTableField(authCtx, int32(c.Int("table-id"))).
+				FieldCreateField(client.FieldCreateField{
+					TextFieldCreateField: client.NewTextFieldCreateField(
+						field,
+						client.TEXT,
+					),
+				}).Execute()
+			if err != nil {
+				return cli.Exit(
+					fmt.Errorf(
+						"error in creating table field %s %s",
+						field, err,
+					),
+					2,
+				)
+			}
+			defer frsp.Body.Close()
+		}
+	}
+	return nil
+}
+
 func CreateTable(c *cli.Context) error {
 	logger := registry.GetLogger()
 	bclient := baserowClient(c.String("server"))
@@ -18,13 +82,14 @@ func CreateTable(c *cli.Context) error {
 		c.String("token"),
 	)
 	tbl, resp, err := bclient.DatabaseTablesApi.CreateDatabaseTable(authCtx, int32(c.Int("database-id"))).
+		TableCreate(client.TableCreate{Name: c.String("table")}).
 		Execute()
-	defer resp.Body.Close()
 	if err != nil {
 		return cli.Exit(
 			fmt.Errorf("error in creating table %s", err), 2,
 		)
 	}
+	defer resp.Body.Close()
 	logger.Infof("created table %s", tbl.GetName())
 	return nil
 }
@@ -44,8 +109,23 @@ func CreateTableFlag() []cli.Flag {
 		},
 		&cli.StringFlag{
 			Name:     "table",
-			Aliases:  []string{"t"},
 			Usage:    "Database table",
+			Required: true,
+		},
+	}
+}
+
+func LoadOntologyToTableFlag() []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:     "token",
+			Aliases:  []string{"t"},
+			Usage:    "database token with write privilege",
+			Required: true,
+		},
+		&cli.IntFlag{
+			Name:     "table-id",
+			Usage:    "Database table id",
 			Required: true,
 		},
 	}
