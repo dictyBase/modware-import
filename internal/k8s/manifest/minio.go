47ef362ae0b0f49ea3512d3d520815d7e1b72864
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-08-16 14:25:28 -0500

    fix(minio.go): change the secret name for SECRET_KEY environment variable
    The secret name for the SECRET_KEY environment variable was changed
    to "dictycr-sercet-dev" from "dictycr-secret-dev".

--- internal/k8s/manifest/minio.go
+++ internal/k8s/manifest/minio.go
@@ -10,7 +10,7 @@ func MinioEnv() []apiv1.EnvVar {
 			Name: "ACCESS_KEY",
 			ValueFrom: &apiv1.EnvVarSource{
 				SecretKeyRef: &apiv1.SecretKeySelector{
-					LocalObjectReference: apiv1.LocalObjectReference{Name: "dictybase-configuration"},
+					LocalObjectReference: apiv1.LocalObjectReference{Name: "dictycr-secret-dev"},
 					Key:                  "minio.accesskey",
 				},
 			},
@@ -19,7 +19,7 @@ func MinioEnv() []apiv1.EnvVar {
 			Name: "SECRET_KEY",
 			ValueFrom: &apiv1.EnvVarSource{
 				SecretKeyRef: &apiv1.SecretKeySelector{
-					LocalObjectReference: apiv1.LocalObjectReference{Name: "dictybase-configuration"},
+					LocalObjectReference: apiv1.LocalObjectReference{Name: "dictycr-sercet-dev"},
 					Key:                  "minio.secretkey",
 				},
 			},
