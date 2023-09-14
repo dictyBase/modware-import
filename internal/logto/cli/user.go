e10792e8fefa5712a04d066d9b5f4a227f53c53e
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-09-14 08:59:04 -0500

    feat(cli): add ImportUser function to handle user import functionality
    The ImportUser function is added to the cli package. This function is
    responsible for importing a user into the system. It uses the logto
    client package to interact with the logto service. The function takes in
    a cli.Context as a parameter. It retrieves the logger from the registry,
    creates a new logto client, and obtains an access token using the
    provided app ID, app secret, and API resource. It then proceeds to
    create a new user using the logto client's CreateUser method, passing in
    the necessary user information. If any errors occur during the process,
    the function returns an error message. Otherwise, it logs the user ID
    and returns nil.

---
+++ internal/logto/cli/user.go
@@ -0,0 +1,35 @@
+package cli
+
+import (
+	logto "github.com/dictyBase/modware-import/internal/logto/client"
+	"github.com/dictyBase/modware-import/internal/registry"
+	"github.com/urfave/cli/v2"
+)
+
+func ImportUser(cltx *cli.Context) error {
+	logger := registry.GetLogger()
+	lclient := logto.NewClient(cltx.String("endpoint"))
+	atoken, err := lclient.AccessToken(
+		cltx.String("app-id"),
+		cltx.String("app-secret"),
+		cltx.String("api-resource"),
+	)
+	if err != nil {
+		return cli.Exit(err.Error(), 2)
+	}
+	userId, err := lclient.CreateUser(
+		atoken,
+		&logto.APIUsersPostReq{
+			PrimaryEmail: "bola@bola.com",
+			PrimaryPhone: "19343049303438",
+			Username:     "hello",
+			Password:     "r93r938493*7043",
+			Name:         "bola",
+		},
+	)
+	if err != nil {
+		return cli.Exit(err.Error(), 2)
+	}
+	logger.Infof("got user id %s\n", userId)
+	return nil
+}
