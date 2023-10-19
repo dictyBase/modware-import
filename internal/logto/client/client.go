529f614f7fea5ac90f91562bc9033cf8ad0daa2a
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-10-19 08:50:41 -0500

    chore(client.go): extract commonHeader function to improve code reusability and readability
    The commonHeader function is extracted to improve code reusability and
    readability. It sets the Content-Type, Accept, and Authorization headers
    for an HTTP request. This extraction allows for easier maintenance and
    avoids code duplication when setting headers in multiple places.

--- internal/logto/client/client.go
+++ internal/logto/client/client.go
@@ -224,3 +224,9 @@ func (clnt *Client) CreateUser(
 	}
 	return usr.Id, nil
 }
+
+func commonHeader(lreq *http.Request, token string) {
+	lreq.Header.Set("Content-Type", "application/json")
+	lreq.Header.Set("Accept", "application/json")
+	lreq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+}
