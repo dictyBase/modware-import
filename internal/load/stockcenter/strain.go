ce6befdb55dbc05ccd712c3fbcbfd1dd09d9f014
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-01 17:35:29 -0600

    refactor(strain.go): split LoadStrain function into smaller functions for better readability and maintainability
    The LoadStrain function was too large and complex, making it difficult
    to understand and maintain. It has been refactored into smaller, more
    manageable functions. Each function now has a single responsibility,
    improving the readability and maintainability of the code.

--- internal/load/stockcenter/strain.go
+++ internal/load/stockcenter/strain.go
@@ -17,32 +17,77 @@ import (
 )
 
 func LoadStrain(cmd *cobra.Command, args []string) error {
+	pl, err := initializePublicationSource()
+	if err != nil {
+		return err
+	}
+	gl, err := initializeGeneSource()
+	if err != nil {
+		return err
+	}
+	al, err := initializeAnnotationSource()
+	if err != nil {
+		return err
+	}
+	sr := createCsvStrainReader(al, pl, gl)
+
+	logger := registry.GetLogger()
+	client := regs.GetStockAPIClient()
+
+	count := processStrains(sr, logger, client)
+	logFinalCount(logger, count)
+
+	return nil
+}
+
+func initializePublicationSource() (source.StockPubLookup, error) {
 	pl, err := source.NewStockPubLookup(
 		registry.GetReader(regs.StrainPubReader),
 	)
 	if err != nil {
-		return fmt.Errorf("error in opening publication source %s", err)
+		return nil, fmt.Errorf("error in opening publication source %s", err)
 	}
+	return pl, nil
+}
+
+func initializeGeneSource() (source.StockGeneLookup, error) {
 	gl, err := source.NewStockGeneLookp(
 		registry.GetReader(regs.StrainGeneReader),
 	)
 	if err != nil {
-		return fmt.Errorf("error in opening gene source %s", err)
+		return nil, fmt.Errorf("error in opening gene source %s", err)
 	}
+	return gl, nil
+}
+
+func initializeAnnotationSource() (source.StockAnnotatorLookup, error) {
 	al, err := source.NewStockAnnotatorLookup(
 		registry.GetReader(regs.StrainAnnotatorReader),
 	)
 	if err != nil {
-		return fmt.Errorf("error in opening annotation source %s", err)
+		return nil, fmt.Errorf("error in opening annotation source %s", err)
 	}
-	sr := source.NewCsvStrainReader(
+	return al, nil
+}
+
+func createCsvStrainReader(
+	al source.StockAnnotatorLookup,
+	pl source.StockPubLookup,
+	gl source.StockGeneLookup,
+) source.StrainReader {
+	return source.NewCsvStrainReader(
 		registry.GetReader(regs.StrainReader),
 		al,
 		pl,
 		gl,
 	)
-	logger := registry.GetLogger()
-	client := regs.GetStockAPIClient()
+}
+
+func processStrains(
+	sr source.StrainReader,
+	logger logrus.FieldLogger,
+	client pb.StockServiceClient,
+) int {
 	count := 0
 	for sr.Next() {
 		strain, err := sr.Value()
@@ -53,88 +98,125 @@ func LoadStrain(cmd *cobra.Command, args []string) error {
 			)
 			continue
 		}
-		if len(strain.User) == 0 {
-			logger.Errorf(
-				"strain %s does not have a user assignment, skipping the load",
-				strain.Id,
-			)
-			continue
+		if err := processSingleStrain(strain, logger, client); err != nil {
+			return count
 		}
-		_, err = client.GetStrain(
-			context.Background(),
-			&pb.StockId{Id: strain.Id},
-		)
-		if err != nil {
-			if status.Code(err) == codes.NotFound {
-				// create new strain entry
-				attr := &pb.ExistingStrainAttributes{
-					CreatedAt: aphgrpc.TimestampProto(strain.CreatedOn),
-					UpdatedAt: aphgrpc.TimestampProto(strain.UpdatedOn),
-					CreatedBy: strain.User,
-					UpdatedBy: strain.User,
-					Summary:   strain.Summary,
-					Species:   strain.Species,
-					Label:     strain.Descriptor,
-				}
-				if len(strain.Publications) > 0 {
-					attr.Publications = strain.Publications
-				} else {
-					logger.Warnf("strain %s has no publication entry", strain.Id)
-				}
-				if len(strain.Genes) > 0 {
-					attr.Genes = strain.Genes
-				}
-				nstr, err := client.LoadStrain(
-					context.Background(),
-					&pb.ExistingStrain{
-						Data: &pb.ExistingStrain_Data{
-							Type:       "strain",
-							Id:         strain.Id,
-							Attributes: attr,
-						},
-					})
-				if err != nil {
-					return fmt.Errorf(
-						"error in creating strain %s %s",
-						strain.Id,
-						err,
-					)
-				}
-				logger.Debugf("created strain %s", nstr.Data.Id)
-				count += 1
-				continue
-			}
-			return fmt.Errorf("error in finding strain %s %s", strain.Id, err)
-		}
-		// update strains
-		attr := &pb.StrainUpdateAttributes{
-			UpdatedBy: strain.User,
-			Summary:   strain.Summary,
-			Label:     strain.Descriptor,
-		}
-		if len(strain.Publications) > 0 {
-			attr.Publications = strain.Publications
-		} else {
-			logger.Warnf("strain %s has no publication entry", strain.Id)
-		}
-		if len(strain.Genes) > 0 {
-			attr.Genes = strain.Genes
-		}
-		ustr, err := client.UpdateStrain(
-			context.Background(),
-			&pb.StrainUpdate{
-				Data: &pb.StrainUpdate_Data{
-					Type:       "strain",
-					Id:         strain.Id,
-					Attributes: attr,
-				},
-			})
-		if err != nil {
-			return fmt.Errorf("error in updating strain %s %s", strain.Id, err)
-		}
-		logger.Debugf("updated strain %s", ustr.Data.Id)
 		count += 1
 	}
+	return count
+}
+
+func processSingleStrain(
+	strain *source.Strain,
+	logger logrus.FieldLogger,
+	client pb.StockServiceClient,
+) error {
+	if len(strain.User) == 0 {
+		logger.Errorf(
+			"strain %s does not have a user assignment, skipping the load",
+			strain.Id,
+		)
+		return nil
+	}
+	_, err := client.GetStrain(context.Background(), &pb.StockId{Id: strain.Id})
+	if err != nil {
+		if status.Code(err) == codes.NotFound {
+			return createStrain(strain, logger, client)
+		}
+		return fmt.Errorf("error in finding strain %s %s", strain.Id, err)
+	}
+	return updateStrain(strain, logger, client)
+}
+
+func createStrain(
+	strain *source.Strain,
+	logger logrus.FieldLogger,
+	client pb.StockServiceClient,
+) error {
+	attr := populateStrainAttributes(strain, logger)
+	nstr, err := client.LoadStrain(
+		context.Background(),
+		&pb.ExistingStrain{
+			Data: &pb.ExistingStrain_Data{
+				Type:       "strain",
+				Id:         strain.Id,
+				Attributes: attr,
+			},
+		})
+	if err != nil {
+		return fmt.Errorf("error in creating strain %s %s", strain.Id, err)
+	}
+	logger.Debugf("created strain %s", nstr.Data.Id)
+	return nil
+}
+
+func updateStrain(
+	strain *source.Strain,
+	logger logrus.FieldLogger,
+	client pb.StockServiceClient,
+) error {
+	attr := populateStrainUpdateAttributes(strain, logger)
+	ustr, err := client.UpdateStrain(
+		context.Background(),
+		&pb.StrainUpdate{
+			Data: &pb.StrainUpdate_Data{
+				Type:       "strain",
+				Id:         strain.Id,
+				Attributes: attr,
+			},
+		})
+	if err != nil {
+		return fmt.Errorf("error in updating strain %s %s", strain.Id, err)
+	}
+	logger.Debugf("updated strain %s", ustr.Data.Id)
+	return nil
+}
+
+func populateStrainAttributes(
+	strain *source.Strain,
+	logger logrus.FieldLogger,
+) *pb.ExistingStrainAttributes {
+	attr := &pb.ExistingStrainAttributes{
+		CreatedAt: aphgrpc.TimestampProto(strain.CreatedOn),
+		UpdatedAt: aphgrpc.TimestampProto(strain.UpdatedOn),
+		CreatedBy: strain.User,
+		UpdatedBy: strain.User,
+		Summary:   strain.Summary,
+		Species:   strain.Species,
+		Label:     strain.Descriptor,
+	}
+	if len(strain.Publications) > 0 {
+		attr.Publications = strain.Publications
+	} else {
+		logger.Warnf("strain %s has no publication entry", strain.Id)
+	}
+	if len(strain.Genes) > 0 {
+		attr.Genes = strain.Genes
+	}
+	return attr
+}
+
+func populateStrainUpdateAttributes(
+	strain *source.Strain,
+	logger logrus.FieldLogger,
+) *pb.StrainUpdateAttributes {
+	attr := &pb.StrainUpdateAttributes{
+		UpdatedBy: strain.User,
+		Summary:   strain.Summary,
+		Label:     strain.Descriptor,
+	}
+	if len(strain.Publications) > 0 {
+		attr.Publications = strain.Publications
+	} else {
+		logger.Warnf("strain %s has no publication entry", strain.Id)
+	}
+	if len(strain.Genes) > 0 {
+		attr.Genes = strain.Genes
+	}
+	return attr
+}
+
+func logFinalCount(logger logrus.FieldLogger, count int) {
 	logger.WithFields(
 		logrus.Fields{
 			"type":  "annotations",
@@ -142,5 +224,4 @@ func LoadStrain(cmd *cobra.Command, args []string) error {
 			"event": "load",
 			"count": count,
 		}).Infof("loaded strain annotations")
-	return nil
 }
