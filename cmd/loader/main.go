cf9a44570da64bbcf13a28d2fc81485126f5712f
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-02 15:57:28 -0600

    feat(main.go): add new loader command line application
    A new command line application named 'loader' has been added. This
    application is designed for loading data. It includes various flags for
    setting the log level, log format, and log file. It also includes a
    'content-data' command for loading content data from s3 storage.

---
+++ cmd/loader/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	contentcli "github.com/dictyBase/modware-import/internal/content/cli"
+	"github.com/dictyBase/modware-import/internal/content/client"
+	"github.com/dictyBase/modware-import/internal/logger"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:   "loader",
+		Usage:  "A command line application for loading data",
+		Before: logger.SetupCliLogger,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "log-level",
+				Usage: "Logging level, should be one of debug,warn,info or error",
+				Value: "error",
+			},
+			&cli.StringFlag{
+				Name:  "log-format",
+				Usage: "Format of log, either of json or text",
+				Value: "json",
+			},
+			&cli.StringFlag{
+				Name:  "log-file",
+				Usage: "log file for output in addition to stderr",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:   "content-data",
+				Usage:  "load content data from s3 storage",
+				Action: contentcli.LoadContent,
+				Flags:  contentcli.ContentLoaderFlags(),
+				Before: client.CliSetup,
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
