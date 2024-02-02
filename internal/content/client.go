e88045ed7c647c0a6b1d0138def0d2af9b5bf1e2
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-02 13:07:48 -0600

    feat(client.go): add SetS3Client function to initialize S3 client
    The SetS3Client function is added to initialize the S3 client. This
    function is necessary to allow the application to interact with S3
    storage, which is crucial for handling file uploads and downloads. The
    function takes a cli.Context as an argument, creates a new S3 client,
    and sets it in the registry.

--- internal/content/client.go
+++ internal/content/client.go
@@ -4,11 +4,23 @@ import (
 	"fmt"
 
 	"github.com/dictyBase/go-genproto/dictybaseapis/content"
+	"github.com/dictyBase/modware-import/internal/datasource/s3"
+	regsc "github.com/dictyBase/modware-import/internal/registry"
 	registry "github.com/dictyBase/modware-import/internal/registry/stockcenter"
 	"github.com/urfave/cli/v2"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
+
+func SetS3Client(cltx *cli.Context) error {
+	client, err := s3.NewCliS3Client(cltx)
+	if err != nil {
+		return fmt.Errorf("error in getting instance of s3 client %s", err)
+	}
+	regsc.SetS3Client(client)
+
+	return nil
+}
 
 func SetContentAPIClient(cltx *cli.Context) error {
 	conn, err := grpc.Dial(
