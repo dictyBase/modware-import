eb6fd00a7bf2ddd88467e812e13411305c4939c6
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-20 06:54:36 -0600

    refactor: move database related functions from cli to database package
    The database related functions were moved from the cli package to a new
    database package. This change was made to improve the organization of
    the codebase, making it easier to maintain and understand. The database
    package now encapsulates all the database related operations, providing
    a clear separation of concerns.

---
+++ internal/baserow/database/database.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"net/http"
+
+	"github.com/dictyBase/modware-import/internal/baserow/client"
+)
+
+type AccessTokenProperties struct {
+	Email, Password, Server string
+}
+
+func BaserowClient(server string) *client.APIClient {
+	conf := client.NewConfiguration()
+	conf.Host = server
+	conf.Scheme = "https"
+	return client.NewAPIClient(conf)
+}
+
+func AccessToken(args *AccessTokenProperties) (string, error) {
+	req := BaserowClient(
+		args.Server,
+	).UserApi.TokenAuth(
+		context.Background(),
+	)
+	resp, r, err := req.TokenObtainPairWithUser(
+		client.TokenObtainPairWithUser{
+			Email:    &args.Email,
+			Password: args.Password,
+		},
+	).Execute()
+	defer r.Body.Close()
+	if err != nil {
+		return "", fmt.Errorf("error in executing API call %s", err)
+	}
+	if r != nil && r.StatusCode == http.StatusUnauthorized {
+		return "", errors.New("unauthrorized access")
+	}
+	return resp.GetToken(), nil
+}
