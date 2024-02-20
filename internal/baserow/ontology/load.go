3111461e9158d6842924f46f3ca3ed5c9884ea26
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-20 16:43:03 -0600

    feat(load.go): add reqToResponse function to handle HTTP requests and responses
    The reqToResponse function has been added to handle HTTP requests and
    responses. This function makes a request using the provided http.Request
    object and returns the response. It also handles errors during the
    request and non-200 status codes in the response, providing more
    detailed error messages in these cases.

--- internal/baserow/ontology/load.go
+++ internal/baserow/ontology/load.go
@@ -1,6 +1,32 @@
 import (
+	"fmt"
+	"io"
 	"net/http"
 )
+func reqToResponse(creq *http.Request) (*http.Response, error) {
+	client := &http.Client{}
+	uresp, err := client.Do(creq)
+	if err != nil {
+		return uresp, fmt.Errorf("error in making request %s", err)
+	}
+	if uresp.StatusCode != 200 {
+		cnt, err := io.ReadAll(uresp.Body)
+		if err != nil {
+			return uresp, fmt.Errorf(
+				"error in response and the reading the body %d %s",
+				uresp.StatusCode,
+				err,
+			)
+		}
+		return uresp, fmt.Errorf(
+			"unexpected error response %d %s",
+			uresp.StatusCode,
+			string(cnt),
+		)
+	}
+	return uresp, nil
+}
+
 func commonHeader(lreq *http.Request, token string) {
 	lreq.Header.Set("Content-Type", "application/json")
 	lreq.Header.Set("Accept", "application/json")
