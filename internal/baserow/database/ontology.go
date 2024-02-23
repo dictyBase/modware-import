58342b56a47548a37c81b1fe091b79d86fba7edd
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-23 11:07:36 -0600

    refactor(ontology.go): simplify OntologyTableManager struct and remove unused functions
    The OntologyTableManager struct has been simplified by embedding the
    TableManager struct, removing the need to duplicate fields. The
    CreateTable function has been removed as it was not being used. The
    fieldInformation and toFieldNames functions have also been removed as
    they were not being used. The fieldDefs function has been renamed to
    FieldDefs and made public as it is now being used in other parts of the
    code.

--- internal/baserow/database/ontology.go
+++ internal/baserow/database/ontology.go
@@ -2,49 +2,49 @@ package database
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+
 	"slices"
 
 	"github.com/dictyBase/modware-import/internal/baserow/client"
 	"github.com/dictyBase/modware-import/internal/baserow/httpapi"
 	"github.com/dictyBase/modware-import/internal/collection"
-	"github.com/sirupsen/logrus"
 )
 
 type OntologyTableManager struct {
-	Logger     *logrus.Entry
-	Client     *client.APIClient
-	Ctx        context.Context
-	Token      string
-	DatabaseId int32
+	*TableManager
 }
 
 type tableFieldsResponse struct {
 	Name string `json:"name"`
 }
 
-func (ont *OntologyTableManager) CreateTable(
-	table string,
-) (*client.Table, error) {
-	var row []interface{}
-	row = append(row, []string{""})
-	tbl, resp, err := ont.Client.
-		DatabaseTablesApi.
-		CreateDatabaseTable(ont.Ctx, ont.DatabaseId).
-		TableCreate(client.TableCreate{Name: table, Data: row}).
-		Execute()
-	if err != nil {
-		return tbl, fmt.Errorf(
-			"error in creating table %s %s",
-			table, err,
-		)
+func (ont *OntologyTableManager) CreateFields(tbl *client.Table) error {
+	reqURL := fmt.Sprintf(
+		"https://%s/api/database/fields/table/%d/",
+		ont.Client.GetConfig().Host,
+		tbl.GetId(),
+	)
+	for _, payload := range FieldDefs() {
+		jsonData, err := json.Marshal(payload)
+		if err != nil {
+			return fmt.Errorf("error in encoding body %s", err)
+		}
+		req, err := http.NewRequest("POST", reqURL, bytes.NewBuffer(jsonData))
+		if err != nil {
+			return fmt.Errorf("error in creating request %s ", err)
+		}
+		httpapi.CommonHeader(req, ont.Token, "JWT")
+		res, err := httpapi.ReqToResponse(req)
+		if err != nil {
+			return err
+		}
+		defer res.Body.Close()
 	}
-	defer resp.Body.Close()
 
-	return tbl, nil
+	return nil
 }
 
 func (ont *OntologyTableManager) CheckAllTableFields(
@@ -84,63 +84,11 @@ func (ont *OntologyTableManager) CheckAllTableFields(
 	return true, nil
 }
 
-func (ont *OntologyTableManager) RemoveInitialFields(tbl *client.Table) error {
-	return nil
-}
-
-func (ont *OntologyTableManager) CreateFields(tbl *client.Table) error {
-	reqURL := fmt.Sprintf(
-		"https://%s/api/database/fields/table/%d/",
-		ont.Client.GetConfig().Host,
-		tbl.GetId(),
-	)
-	for _, payload := range fieldDefs() {
-		jsonData, err := json.Marshal(payload)
-		if err != nil {
-			return fmt.Errorf("error in encoding body %s", err)
-		}
-		req, err := http.NewRequest("POST", reqURL, bytes.NewBuffer(jsonData))
-		if err != nil {
-			return fmt.Errorf("error in creating request %s ", err)
-		}
-		httpapi.CommonHeader(req, ont.Token, "JWT")
-		res, err := httpapi.ReqToResponse(req)
-		if err != nil {
-			return err
-		}
-		defer res.Body.Close()
-	}
-
-	return nil
-}
-
 func FieldNames() []string {
 	return []string{"term_id", "name", "is_obsolete"}
 }
 
-func fieldInformation(field client.FieldField) (int32, string) {
-	if field.TextFieldField != nil {
-		return field.TextFieldField.Id, field.TextFieldField.Name
-	}
-	if field.BooleanFieldField != nil {
-		return field.BooleanFieldField.Id, field.BooleanFieldField.Name
-	}
-	if field.LongTextFieldField != nil {
-		return field.LongTextFieldField.Id, field.LongTextFieldField.Name
-	}
-	return field.DateFieldField.Id, *field.DateFieldField.Name
-}
-
-func toFieldNames(fields []client.FieldField) []string {
-	fieldNames := make([]string, 0)
-	for _, fld := range fields {
-		_, name := fieldInformation(fld)
-		fieldNames = append(fieldNames, name)
-	}
-	return fieldNames
-}
-
-func fieldDefs() []map[string]interface{} {
+func FieldDefs() []map[string]interface{} {
 	return []map[string]interface{}{
 		{"name": "name", "type": "text"},
 		{"name": "term_id", "type": "text"},
