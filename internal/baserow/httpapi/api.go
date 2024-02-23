7e421a327fa306887f4a6c8bf05ccc7a896182f0
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-22 19:53:40 -0600

    feat(httpapi): add new httpapi package with common header and request handling functions
    The new httpapi package includes functions for setting common headers
    and handling HTTP requests. This centralizes the HTTP request handling
    logic, making it easier to maintain and update. The CommonHeader
    function sets standard headers for JSON content and authorization. The
    ReqToResponse function sends the request and handles any errors,
    including non-200 status codes. This improves error handling by
    providing more detailed error messages.

---
+++ internal/baserow/httpapi/api.go
@@ -0,0 +1,37 @@
+package httpapi
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func CommonHeader(lreq *http.Request, token, format string) {
+	lreq.Header.Set("Content-Type", "application/json")
+	lreq.Header.Set("Accept", "application/json")
+	lreq.Header.Set("Authorization", fmt.Sprintf("%s %s", format, token))
+}
+
+func ReqToResponse(creq *http.Request) (*http.Response, error) {
+	client := &http.Client{}
+	uresp, err := client.Do(creq)
+	if err != nil {
+		return uresp, fmt.Errorf("error in making request %s", err)
+	}
+	if uresp.StatusCode != 200 {
+		cnt, err := io.ReadAll(uresp.Body)
+		if err != nil {
+			return uresp, fmt.Errorf(
+				"error in response and the reading the body %d %s",
+				uresp.StatusCode,
+				err,
+			)
+		}
+		return uresp, fmt.Errorf(
+			"unexpected error response %d %s",
+			uresp.StatusCode,
+			string(cnt),
+		)
+	}
+	return uresp, nil
+}
