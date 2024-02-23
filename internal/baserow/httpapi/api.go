0743abe6f01ff8e5029c7142c80b4af68c2d99aa
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-23 15:51:19 -0600

    feat(httpapi/api.go): add JWT and Token authorization support
    Two new functions, SetHeaderWithToken and SetHeaderWithJWT, have been
    added to support JWT and Token authorization respectively. These
    functions set the request headers with the provided token or JWT.

--- internal/baserow/httpapi/api.go
+++ internal/baserow/httpapi/api.go
@@ -4,7 +4,50 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
+	F "github.com/IBM/fp-go/function"
+	H "github.com/IBM/fp-go/http/builder"
+	C "github.com/IBM/fp-go/http/content"
+	HD "github.com/IBM/fp-go/http/headers"
+	S "github.com/IBM/fp-go/string"
 )
+
+var (
+	WithJWT = F.Flow2(
+		S.Format[string]("JWT %s"),
+		H.WithAuthorization,
+	)
+	WithToken = F.Flow2(
+		S.Format[string]("Token %s"),
+		H.WithAuthorization,
+	)
+)
+
+func SetHeaderWithToken(token string) func(*http.Request) *http.Request {
+	return func(req *http.Request) *http.Request {
+		req.Header = F.Pipe3(
+			H.Default,
+			H.WithContentType(C.Json),
+			H.WithHeader(HD.Accept)(C.Json),
+			WithToken(token),
+		).GetHeaders()
+
+		return req
+	}
+}
+
+func SetHeaderWithJWT(jwt string) func(*http.Request) *http.Request {
+	return func(req *http.Request) *http.Request {
+		req.Header = F.Pipe3(
+			H.Default,
+			H.WithContentType(C.Json),
+			H.WithHeader(HD.Accept)(C.Json),
+			WithJWT(jwt),
+		).GetHeaders()
+
+		return req
+	}
+}
 
 func CommonHeader(lreq *http.Request, token, format string) {
 	lreq.Header.Set("Content-Type", "application/json")
