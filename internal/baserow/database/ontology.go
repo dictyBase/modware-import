175151f97320d39be63c10d6d66856b255cc6cc5
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-26 07:12:26 -0600

    feat(ontology.go): add new types and functions to handle field deletion and request feedback
    The changes introduce new types and functions to handle field deletion
    and request feedback in the ontology.go file. This includes the addition
    of new types 'tableFieldDelResponse' and 'fieldsReqFeedback' to handle
    the response from field deletion requests and feedback from field
    requests respectively. New functions 'hasExtraField',
    'onFieldsReqFeedbackError', 'onFieldDelReqFeedbackSuccess',
    'onFieldsReqFeedbackSuccess', and 'onFieldDelReqFeedbackNone' have been
    added to handle various scenarios during field deletion and request
    feedback.

--- internal/baserow/database/ontology.go
+++ internal/baserow/database/ontology.go
@@ -8,6 +8,12 @@ import (
 
 	"slices"
 
+	A "github.com/IBM/fp-go/array"
+	R "github.com/IBM/fp-go/context/readerioeither"
+	H "github.com/IBM/fp-go/context/readerioeither/http"
+	E "github.com/IBM/fp-go/either"
+	F "github.com/IBM/fp-go/function"
+	O "github.com/IBM/fp-go/option"
 	"github.com/dictyBase/modware-import/internal/baserow/client"
 	"github.com/dictyBase/modware-import/internal/baserow/httpapi"
 	"github.com/dictyBase/modware-import/internal/collection"
@@ -19,7 +25,30 @@ type OntologyTableManager struct {
 
 type tableFieldsResponse struct {
 	Name string `json:"name"`
+	Id   int    `json:"id"`
 }
+
+type tableFieldDelResponse struct {
+	RelatedFields []struct {
+		ID      int `json:"id"`
+		TableID int `json:"table_id"`
+	} `json:"related_fields,omitempty"`
+}
+
+type fieldsReqFeedback struct {
+	Error  error
+	Fields []tableFieldsResponse
+	Msg    string
+}
+
+var (
+	readFieldDelResp = H.ReadJson[tableFieldDelResponse](
+		H.MakeClient(http.DefaultClient),
+	)
+	readFieldsResp = H.ReadJson[[]tableFieldsResponse](
+		H.MakeClient(http.DefaultClient),
+	)
+)
 
 func (ont *OntologyTableManager) CreateFields(tbl *client.Table) error {
 	reqURL := fmt.Sprintf(
@@ -83,4 +112,26 @@ func FieldDefs() []map[string]interface{} {
 		{"name": "term_id", "type": "text"},
 		{"name": "is_obsolete", "type": "boolean"},
 	}
+}
+
+func hasExtraField(elem tableFieldsResponse) bool {
+	return elem.Name == "Field 1"
+}
+
+func onFieldsReqFeedbackError(err error) fieldsReqFeedback {
+	return fieldsReqFeedback{Error: err}
+}
+
+func onFieldDelReqFeedbackSuccess(
+	resp tableFieldDelResponse,
+) fieldsReqFeedback {
+	return fieldsReqFeedback{Msg: "deleted extra field"}
+}
+
+func onFieldsReqFeedbackSuccess(resp []tableFieldsResponse) fieldsReqFeedback {
+	return fieldsReqFeedback{Fields: resp}
+}
+
+func onFieldDelReqFeedbackNone() fieldsReqFeedback {
+	return fieldsReqFeedback{Msg: "no field found to delete"}
 }
