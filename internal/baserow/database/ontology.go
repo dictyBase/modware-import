620e59dfc38f69354aa423dc840222f1580afe9e
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-28 15:25:24 -0600

    feat(ontology.go): add TabledateParams function to manage table date parameters
    The TabledateParams function was added to manage the parameters related
    to dates in the table. This includes parameters for 'is_obsolete',
    'last_modified', and 'created_on'. This function will help in better
    management and manipulation of date-related data in the table, providing
    more control over how date and time information is handled and
    displayed.

--- internal/baserow/database/ontology.go
+++ internal/baserow/database/ontology.go
@@ -15,3 +15,29 @@ func (ont *OntologyTableManager) FieldDefs() []map[string]interface{} {
 		{"name": "is_obsolete", "type": "boolean"},
 	}
 }
+
+func (ont *OntologyTableManager) TabledateParams() map[string]map[string]interface{} {
+	paramsMap := make(map[string]map[string]interface{})
+	paramsMap["is_obsolete"] = map[string]interface{}{
+		"name": "is_obsolete",
+		"type": "boolean",
+	}
+	paramsMap["last_modified"] = map[string]interface{}{
+		"name":              "last_modified",
+		"type":              "created_on",
+		"date_format":       "US",
+		"date_include_time": "true",
+		"date_time_format":  "12",
+		"date_show_tzinfo":  "true",
+	}
+	paramsMap["created_on"] = map[string]interface{}{
+		"name":              "created_on",
+		"type":              "created_on",
+		"date_format":       "US",
+		"date_include_time": "true",
+		"date_time_format":  "12",
+		"date_show_tzinfo":  "true",
+	}
+
+	return paramsMap
+}
