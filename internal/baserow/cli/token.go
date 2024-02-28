4858dcd98adcef07ddb696aed9548285548f927f
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-28 17:28:16 -0600

    feat(token.go): add refreshToken function to refresh authentication tokens
    The refreshToken function has been added to the token.go file. This
    function is responsible for refreshing the authentication tokens when
    they expire. It uses the NewTokenManager function from the httpapi
    package to create a new token manager and then calls the FreshToken
    method to get a new token.

---
+++ internal/baserow/cli/token.go
@@ -0,0 +1,26 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/dictyBase/modware-import/internal/baserow/httpapi"
+	"github.com/urfave/cli/v2"
+)
+
+func refreshToken(cltx *cli.Context) (string, error) {
+	var empty string
+	tkm, err := httpapi.NewTokenManager(
+		cltx.String("server"),
+		cltx.String("refresh-token-path"),
+	)
+	if err != nil {
+		return empty, err
+	}
+	token, err := tkm.FreshToken()
+	if err != nil {
+		return empty, fmt.Errorf("error in refreshing token %s", err)
+	}
+
+	return token, nil
+
+}
