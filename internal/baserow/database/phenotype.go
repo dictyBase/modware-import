f2cade91b9b2c9c0b5b740360cf5fc1ee2d14a8c
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-28 17:16:15 -0600

    feat(phenotype.go): add annotation_id field and FieldChangeSpecs method to PhenotypeTableManager
    The addition of the "annotation_id" field to the PhenotypeTableManager's
    FieldNames method allows for more detailed tracking of phenotype
    annotations. The new FieldChangeSpecs method provides a map of field
    change specifications, including type and name for each field, and
    additional parameters for date fields. This allows for more granular
    control and customization of the phenotype data handling.

--- internal/baserow/database/phenotype.go
+++ internal/baserow/database/phenotype.go
@@ -6,6 +6,7 @@ type PhenotypeTableManager struct {
 
 func (pheno *PhenotypeTableManager) FieldNames() []string {
 	return []string{
+		"annotation_id",
 		"strain_id",
 		"strain_descriptor",
 		"phenotype_term",
@@ -17,4 +18,38 @@ func (pheno *PhenotypeTableManager) FieldNames() []string {
 		"created_on",
 		"last_modified",
 	}
+}
+
+func (pheno *PhenotypeTableManager) FieldChangeSpecs() map[string]map[string]interface{} {
+	paramsMap := make(map[string]map[string]interface{})
+	paramsMap["annotation_id"] = map[string]interface{}{
+		"name": "annotation_id",
+		"type": "uuid",
+	}
+	paramsMap["assigned_by"] = map[string]interface{}{
+		"name": "assigned_by",
+		"type": "created_by",
+	}
+	paramsMap["deleted"] = map[string]interface{}{
+		"name": "deleted",
+		"type": "boolean",
+	}
+	paramsMap["last_modified"] = map[string]interface{}{
+		"name":              "last_modified",
+		"type":              "created_on",
+		"date_format":       "US",
+		"date_include_time": "true",
+		"date_time_format":  "12",
+		"date_show_tzinfo":  "true",
+	}
+	paramsMap["created_on"] = map[string]interface{}{
+		"name":              "created_on",
+		"type":              "created_on",
+		"date_format":       "US",
+		"date_include_time": "true",
+		"date_time_format":  "12",
+		"date_show_tzinfo":  "true",
+	}
+
+	return paramsMap
 }
