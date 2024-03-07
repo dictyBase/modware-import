74065117938f60e50562110f60f38edc4d78cec8
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-03-07 14:57:48 -0600

    refactor(cli): move CreatePhenoTableHandler function from action.go to phenotype.go
    The CreatePhenoTableHandler function was moved from action.go to
    phenotype.go to improve code organization.

--- internal/baserow/cli/action.go
+++ internal/baserow/cli/action.go
@@ -98,65 +98,6 @@ func CreateAccessToken(cltx *cli.Context) error {
 	return nil
 }
 
-func CreatePhenoTableHandler(cltx *cli.Context) error {
-	token := cltx.String("token")
-	if len(token) == 0 {
-		rtoken, err := refreshToken(cltx)
-		if err != nil {
-			return cli.Exit(err.Error(), 2)
-		}
-		token = rtoken
-	}
-	authCtx := context.WithValue(
-		context.Background(),
-		client.ContextAccessToken,
-		token,
-	)
-	logger := registry.GetLogger()
-	phenoTbl := &database.PhenotypeTableManager{
-		TableManager: &database.TableManager{
-			Client:     database.BaserowClient(cltx.String("server")),
-			Logger:     logger,
-			Ctx:        authCtx,
-			Token:      token,
-			DatabaseId: int32(cltx.Int("database-id")),
-		},
-	}
-	name := cltx.String("table")
-	tbl, err := phenoTbl.CreateTable(name, phenoTbl.FieldNames())
-	if err != nil {
-		return cli.Exit(fmt.Sprintf("error in creating table %s", err), 2)
-	}
-	logger.Infof("created table with fields %s", tbl.GetName())
-	flagNames := []string{
-		"assay-ontology-table",
-		"phenotype-ontology-table",
-		"env-ontology-table",
-	}
-	tableIdMaps, err := allTableIds(phenoTbl.TableManager, flagNames, cltx)
-	if err != nil {
-		return cli.Exit(fmt.Sprintf("error in getting table ids %s", err), 2)
-	}
-	for fieldName, spec := range mergeFieldDefs(
-		phenoTbl.LinkFieldChangeSpecs(tableIdMaps),
-		phenoTbl.FieldChangeSpecs(),
-	) {
-		msg, err := phenoTbl.UpdateField(tbl, fieldName, spec)
-		if err != nil {
-			return cli.Exit(
-				fmt.Sprintf(
-					"error in updating %s field %s",
-					fieldName,
-					err,
-				),
-				2,
-			)
-		}
-		logger.Info(msg)
-	}
-	return nil
-}
-
 func allTableIds(
 	tbm *database.TableManager,
 	flagNames []string,
