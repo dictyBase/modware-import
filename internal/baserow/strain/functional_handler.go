2e3cabd2c7642dbb4a57de10440f26e37eae33a0
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-03-12 15:01:21 -0500

    feat(functional_handler.go): add new handlers and functions for strain data processing
    The changes include the addition of new handlers and functions to
    process strain data. These handlers and functions are designed to handle
    mutagenesis ID, characteristic IDs, and genetic modification ID. They
    also include an initial payload function to set up the initial payload
    for the strain data. The changes also include functions to process
    ontology term ID and convert the loader to payload.

--- internal/baserow/strain/functional_handler.go
+++ internal/baserow/strain/functional_handler.go
@@ -3,9 +3,16 @@ package strain
 import (
 	"fmt"
 	"net/http"
+	"strings"
+
+	J "github.com/IBM/fp-go/json"
+
+	F "github.com/IBM/fp-go/function"
 
 	H "github.com/IBM/fp-go/context/readerioeither/http"
+	E "github.com/IBM/fp-go/either"
 	"github.com/dictyBase/modware-import/internal/baserow/httpapi"
+	"github.com/dictyBase/modware-import/internal/datasource/xls/strain"
 )
 
 var (
@@ -13,6 +20,67 @@ var (
 		H.MakeClient(http.DefaultClient),
 	)
 )
+
+var mutagenesisIdHandler = F.Curry2(
+	func(mutId int, loader *StrainLoader) *StrainLoader {
+		if mutId != 0 {
+			loader.Payload.GeneticModificationId = []int{mutId}
+		}
+
+		return loader
+	})
+
+var charIdsHandler = F.Curry2(
+	func(charIds []int, loader *StrainLoader) *StrainLoader {
+		if len(charIds) != 0 {
+			loader.Payload.GeneticModificationId = charIds
+		}
+
+		return loader
+	})
+
+var genModIdHandler = F.Curry2(
+	func(genmodId int, loader *StrainLoader) *StrainLoader {
+		if genmodId != 0 {
+			loader.Payload.GeneticModificationId = []int{genmodId}
+		}
+
+		return loader
+	})
+
+var initialPayload = F.Curry2(
+	func(loader *StrainLoader, strn *strain.StrainAnnotation) *StrainLoader {
+		payload := &StrainPayload{
+			Descriptor: strn.Descriptor(),
+			Reference:  strn.Reference(),
+			Species:    strn.Species(),
+			Summary:    strn.Summary(),
+		}
+		if strn.HasName() {
+			payload.Names = strn.Name()
+		}
+		if strn.HasSystematicName() {
+			payload.SystematicName = strn.SystematicName()
+		}
+		if strn.HasPlasmid() {
+			payload.Plasmid = strn.Plasmid()
+		}
+		if strn.HasParentId() {
+			payload.ParentId = strn.ParentId()
+		}
+		if strn.HasGenes() {
+			payload.Genes = strn.Genes()
+		}
+		if strn.HasGenotype() {
+			payload.Genotype = strn.Genotype()
+		}
+		if strn.HasDepositor() {
+			payload.Depositor = strn.Depositor()
+		}
+		loader.Payload = payload
+
+		return loader
+	})
 
 type strainCreateResp struct {
 	AnnoId string `json:"annotation_id"`
@@ -28,4 +96,54 @@ func onStrainCreateFeedbackSuccess(
 
 func onStrainCreateFeedbackError(err error) httpapi.ResponseFeedback {
 	return httpapi.ResponseFeedback{Err: err}
+}
+
+func processOntologyTermId(val string) string {
+	return strings.Replace(val, ":", "_", 1)
+}
+
+func mutagenesisId(loader *StrainLoader) E.Either[error, int] {
+	mutId, err := loader.TableManager.SearchRows(
+		processOntologyTermId(loader.Annotation.MutagenesisMethod()),
+		loader.OntologyTableMap["mutagenesis-method-ontology-table"],
+	)
+	if err != nil {
+		return E.Left[int](err)
+	}
+
+	return E.Right[error](mutId)
+}
+
+func genmodId(loader *StrainLoader) E.Either[error, int] {
+	mutId, err := loader.TableManager.SearchRows(
+		processOntologyTermId(loader.Annotation.GeneticModification()),
+		loader.OntologyTableMap["genetic-mod-ontology-table"],
+	)
+	if err != nil {
+		return E.Left[int](err)
+	}
+
+	return E.Right[error](mutId)
+}
+
+func characteristicIds(loader *StrainLoader) E.Either[error, []int] {
+	charIds := make([]int, 0)
+	for _, charac := range strings.Split(loader.Annotation.Characteristic(), ",") {
+		cid, err := loader.TableManager.SearchRows(
+			processOntologyTermId(charac),
+			loader.OntologyTableMap["strainchar-ontology-table"],
+		)
+		if err != nil {
+			return E.Left[[]int](err)
+		}
+		charIds = append(charIds, cid)
+	}
+
+	return E.Right[error](charIds)
+}
+
+func loaderToPayload(ldr *StrainLoader) *StrainPayload { return ldr.Payload }
+
+func marshalPayload(payload *StrainPayload) E.Either[error, []byte] {
+	return F.Pipe1(payload, J.Marshal)
 }
