1fe2a15521693d18d5e4951e80a1f647a682c694
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-03-12 19:59:00 -0500

    refactor(strain.go): move logger initialization to the top of CreateStrainTableHandler function
    The LoadStrainAnnotationToTable function now includes an authentication
    context and a table manager. This allows the function to handle
    authenticated requests and manage database tables, providing more
    functionality and control over the data loading process.
    The logger initialization in the CreateStrainTableHandler function has
    been moved to the top of the function for better readability and
    consistency. This ensures that the logger is initialized before any
    other operations, which can help with debugging and understanding the
    flow of the function.

--- internal/baserow/cli/strain.go
+++ internal/baserow/cli/strain.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/dictyBase/modware-import/internal/baserow/client"
 	"github.com/dictyBase/modware-import/internal/baserow/database"
+
 	"github.com/dictyBase/modware-import/internal/baserow/strain"
 	strainReader "github.com/dictyBase/modware-import/internal/datasource/xls/strain"
 	"github.com/dictyBase/modware-import/internal/registry"
@@ -23,11 +24,29 @@ func flagNames() []string {
 
 func LoadStrainAnnotationToTable(cltx *cli.Context) error {
 	logger := registry.GetLogger()
+	authCtx := context.WithValue(
+		context.Background(),
+		client.ContextAccessToken,
+		cltx.String("token"),
+	)
+	tbm := &database.TableManager{
+		Client:     database.BaserowClient(cltx.String("server")),
+		DatabaseId: int32(cltx.Int("database-id")),
+		Logger:     logger,
+		Ctx:        authCtx,
+		Token:      cltx.String("token"),
+	}
+	tableIdMaps, err := allTableIds(tbm, flagNames(), cltx)
+	if err != nil {
+		return cli.Exit(fmt.Sprintf("error in getting table ids %s", err), 2)
+	}
 	loader := strain.NewStrainLoader(
 		cltx.String("server"),
 		cltx.String("token"),
 		cltx.Int("table-id"),
 		logger,
+		tableIdMaps,
+		tbm,
 	)
 	reader, err := strainReader.NewStrainAnnotationReader(
 		cltx.String("input"),
@@ -45,6 +64,7 @@ func LoadStrainAnnotationToTable(cltx *cli.Context) error {
 }
 
 func CreateStrainTableHandler(cltx *cli.Context) error {
+	logger := registry.GetLogger()
 	token := cltx.String("token")
 	if len(token) == 0 {
 		rtoken, err := refreshToken(cltx)
@@ -58,7 +78,6 @@ func CreateStrainTableHandler(cltx *cli.Context) error {
 		client.ContextAccessToken,
 		token,
 	)
-	logger := registry.GetLogger()
 	strainTbl := &database.StrainTableManager{
 		TableManager: &database.TableManager{
 			Client:     database.BaserowClient(cltx.String("server")),
