aae168c7e0f8dc1fe31af329e6dbcb9f8f1b999d
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-03-15 14:32:23 -0500

    feat(workspace.go): add ListWorkspaces function to fetch list of workspaces
    The ListWorkspaces function is added to the WorkspaceManager struct.
    This function fetches a list of workspaces from the Baserow API. It uses
    the functional programming library to create a pipeline of operations,
    including making a GET request, setting the header with JWT token,
    reading the response, and handling errors.

--- internal/baserow/database/workspace.go
+++ internal/baserow/database/workspace.go
@@ -1,7 +1,14 @@
 package database
 
 import (
+	"context"
 	"fmt"
+
+	R "github.com/IBM/fp-go/context/readerioeither"
+	H "github.com/IBM/fp-go/context/readerioeither/http"
+	E "github.com/IBM/fp-go/either"
+	F "github.com/IBM/fp-go/function"
+	"github.com/dictyBase/modware-import/internal/baserow/httpapi"
 
 	"github.com/sirupsen/logrus"
 )
@@ -14,4 +21,22 @@ type WorkspaceManager struct {
 
 func (wkm *WorkspaceManager) ListWorkspaceURL() string {
 	return fmt.Sprintf("https://%s/api/workspaces/", wkm.Host)
+}
+
+func (wkm *WorkspaceManager) ListWorkspaces() ([]ListResponse, error) {
+	resp := F.Pipe3(
+		wkm.ListWorkspaceURL(),
+		H.MakeGetRequest,
+		R.Map(httpapi.SetHeaderWithJWT(wkm.Token)),
+		readListResp,
+	)(context.Background())
+	output := F.Pipe1(
+		resp(),
+		E.Fold[error, []ListResponse, listReqFeedback](
+			onListReqFeedbackError,
+			onListReqFeedbackSuccess,
+		),
+	)
+
+	return output.Resp, output.Error
 }
