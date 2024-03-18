36398ceb699cea5a54c68e266317090ac0ac8854
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-03-17 19:25:43 -0500

    feat(workspace.go): add function to search workspace user by email
    This change introduces a new function to search for a workspace user by
    their email. This function is necessary to provide a way to look up
    users in a workspace by their email address.

--- internal/baserow/database/workspace.go
+++ internal/baserow/database/workspace.go
@@ -23,6 +23,34 @@ func (wkm *WorkspaceManager) ListWorkspaceURL() string {
 	return fmt.Sprintf("https://%s/api/workspaces/", wkm.Host)
 }
 
+func (wkm *WorkspaceManager) ListWorkspaceUserURL(id int, email string) string {
+	return fmt.Sprintf(
+		"https://%s/api/workspaces/users/workspace/%d?search=%s",
+		wkm.Host, id, email,
+	)
+}
+
+func (wkm *WorkspaceManager) SearchWorkspaceUser(
+	workspaceId int,
+	email string,
+) (int, error) {
+	resp := F.Pipe3(
+		wkm.ListWorkspaceUserURL(workspaceId, email),
+		H.MakeGetRequest,
+		R.Map(httpapi.SetHeaderWithJWT(wkm.Token)),
+		readWorkspaceUserResp,
+	)(context.Background())
+	output := F.Pipe1(
+		resp(),
+		E.Fold[error, []WorkspaceUserResp, workspaceUserFeedback](
+			onWorkspaceUserReqFeedbackError,
+			onWorkspaceUserReqFeedbackSuccess,
+		),
+	)
+
+	return output.Resp[0].UserId, output.Error
+}
+
 func (wkm *WorkspaceManager) ListWorkspaces() ([]ListResponse, error) {
 	resp := F.Pipe3(
 		wkm.ListWorkspaceURL(),
