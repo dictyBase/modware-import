14126f44489ea52719b5b8dc711fa1cdd8d6e244
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-03-20 17:05:33 -0500

    refactor(load.go): restructure PhenotypeLoader and related types for better data handling
    The PhenotypeLoader and related types have been restructured to better
    handle data and improve code readability. The PhenotypeLoader now
    includes additional fields such as Workspace, OntologyTableMap,
    TableManager, and WorkspaceManager. This allows for more detailed data
    handling and manipulation within the loader.
    The PhenotypePayload has also been expanded to include more fields. This
    allows for a more detailed representation of the phenotype data,
    including strain information, assay and environment IDs, reference,
    deletion status, creation time, and assigned by information. This change
    provides a more comprehensive data structure for handling phenotype
    data.

--- internal/baserow/phenotype/load.go
+++ internal/baserow/phenotype/load.go
@@ -1,39 +1,66 @@
 package phenotype
 
 import (
+	"time"
+
+	"github.com/dictyBase/modware-import/internal/baserow/database"
+	"github.com/dictyBase/modware-import/internal/datasource/xls/phenotype"
 	"github.com/sirupsen/logrus"
 )
 
-type LoaderProperties struct {
-	File    string
-	TableId int
-	Token   string
-	Host    string
-	Logger  *logrus.Entry
+const ConcurrentPhenoLoader = 10
+
+type AssignedBy struct {
+	Id int `json:"id"`
+}
+
+type PhenotypePayload struct {
+	StrainId         string       `json:"strain_id"`
+	StrainDescriptor string       `json:"strain_descriptor"`
+	Id               []int        `json:"phenotype_id"`
+	AssayId          []int        `json:"assay_id"`
+	EnvironmentId    []int        `json:"environment_id"`
+	Reference        string       `json:"reference"`
+	Deleted          bool         `json:"deleted"`
+	CreatedOn        time.Time    `json:"created_on"`
+	AssignedBy       []AssignedBy `json:"assigned_by"`
 }
 
 type PhenotypeLoader struct {
-	file    string
-	tableId int
-	token   string
-	host    string
-	logger  *logrus.Entry
+	Workspace        string
+	Host             string
+	Token            string
+	TableId          int
+	Logger           *logrus.Entry
+	OntologyTableMap map[string]int
+	TableManager     *database.TableManager
+	Payload          *PhenotypePayload
+	Annotation       *phenotype.PhenotypeAnnotation
+	WorkspaceManager *database.WorkspaceManager
 }
 
-func NewPhenotypeLoader(args LoaderProperties) *PhenotypeLoader {
+type PhenotypeLoaderProperties struct {
+	Workspace        string
+	Host             string
+	Token            string
+	TableId          int
+	Logger           *logrus.Entry
+	OntologyTableMap map[string]int
+	TableManager     *database.TableManager
+	Payload          *PhenotypePayload
+	Annotation       *phenotype.PhenotypeAnnotation
+	WorkspaceManager *database.WorkspaceManager
+}
+
+func NewPhenotypeLoader(props *PhenotypeLoaderProperties) *PhenotypeLoader {
 	return &PhenotypeLoader{
-		file:    args.File,
-		tableId: args.TableId,
-		token:   args.Token,
-		host:    args.Host,
-		logger:  args.Logger,
+		Workspace:        props.Workspace,
+		Host:             props.Host,
+		Token:            props.Token,
+		TableId:          props.TableId,
+		Logger:           props.Logger,
+		OntologyTableMap: props.OntologyTableMap,
+		TableManager:     props.TableManager,
+		WorkspaceManager: props.WorkspaceManager,
 	}
-}
-
-type ParseNameToDateFeedback struct {
-	Err error
-}
-
-func (loader *PhenotypeLoader) Load(args *LoaderProperties) error {
-	return nil
 }
