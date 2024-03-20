474405f822fa28624a1a9ab3d5793ebdbfb7f46d
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-03-20 17:08:16 -0500

    refactor(load.go): add omitempty to optional fields in PhenotypePayload struct
    The 'omitempty' option indicates that the field should be omitted from
    the encoding if the field has an empty value, defined as false, 0, a nil
    pointer, a nil interface value, and any empty array, slice, map, or
    string. This makes the JSON output cleaner when these fields are not
    used.

--- internal/baserow/phenotype/load.go
+++ internal/baserow/phenotype/load.go
@@ -15,15 +15,15 @@ type AssignedBy struct {
 }
 
 type PhenotypePayload struct {
-	StrainId         string       `json:"strain_id"`
 	StrainDescriptor string       `json:"strain_descriptor"`
 	Id               []int        `json:"phenotype_id"`
-	AssayId          []int        `json:"assay_id"`
-	EnvironmentId    []int        `json:"environment_id"`
 	Reference        string       `json:"reference"`
-	Deleted          bool         `json:"deleted"`
 	CreatedOn        time.Time    `json:"created_on"`
 	AssignedBy       []AssignedBy `json:"assigned_by"`
+	StrainId         string       `json:"strain_id,omitempty"`
+	AssayId          []int        `json:"assay_id,omitempty"`
+	EnvironmentId    []int        `json:"environment_id,omitempty"`
+	Deleted          bool         `json:"deleted,omitempty"`
 }
 
 type PhenotypeLoader struct {
@@ -31,10 +31,10 @@ type PhenotypeLoader struct {
 	Host             string
 	Token            string
 	TableId          int
-	Logger           *logrus.Entry
 	OntologyTableMap map[string]int
-	TableManager     *database.TableManager
 	Payload          *PhenotypePayload
+	Logger           *logrus.Entry
+	TableManager     *database.TableManager
 	Annotation       *phenotype.PhenotypeAnnotation
 	WorkspaceManager *database.WorkspaceManager
 }
@@ -44,10 +44,10 @@ type PhenotypeLoaderProperties struct {
 	Host             string
 	Token            string
 	TableId          int
-	Logger           *logrus.Entry
 	OntologyTableMap map[string]int
-	TableManager     *database.TableManager
 	Payload          *PhenotypePayload
+	Logger           *logrus.Entry
+	TableManager     *database.TableManager
 	Annotation       *phenotype.PhenotypeAnnotation
 	WorkspaceManager *database.WorkspaceManager
 }
