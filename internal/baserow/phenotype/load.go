0ee28f54a92298e521a9bd7eb5a2aca3fbcb6889
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-03-20 21:31:52 -0500

    feat(load.go): add phenotype loading functionality to PhenotypeLoader
    The PhenotypeLoader now has the ability to add phenotype annotations and
    phenotype rows. This is achieved by creating new instances of
    PhenotypeLoader with the added annotations and then converting these
    loaders to payloads. These payloads are then marshalled and sent as HTTP
    requests to create new phenotype entries in the database. This
    functionality is crucial for loading phenotype data into the
    application.

--- internal/baserow/phenotype/load.go
+++ internal/baserow/phenotype/load.go
@@ -1,9 +1,17 @@
 package phenotype
 
 import (
+	"bytes"
+	"context"
+	"fmt"
 	"time"
 
+	R "github.com/IBM/fp-go/context/readerioeither"
+	E "github.com/IBM/fp-go/either"
+	F "github.com/IBM/fp-go/function"
+
 	"github.com/dictyBase/modware-import/internal/baserow/database"
+	"github.com/dictyBase/modware-import/internal/baserow/httpapi"
 	"github.com/dictyBase/modware-import/internal/datasource/xls/phenotype"
 	"github.com/sirupsen/logrus"
 )
@@ -63,4 +71,65 @@ func NewPhenotypeLoader(props *PhenotypeLoaderProperties) *PhenotypeLoader {
 		TableManager:     props.TableManager,
 		WorkspaceManager: props.WorkspaceManager,
 	}
+}
+
+func (loader *PhenotypeLoader) addPheno(
+	pheno *phenotype.PhenotypeAnnotation,
+) E.Either[error, *PhenotypeLoader] {
+	newLoader := NewPhenotypeLoader(&PhenotypeLoaderProperties{
+		Host:             loader.Host,
+		Token:            loader.Token,
+		Workspace:        loader.Workspace,
+		TableId:          loader.TableId,
+		Logger:           loader.Logger,
+		OntologyTableMap: loader.OntologyTableMap,
+		TableManager:     loader.TableManager,
+		WorkspaceManager: loader.WorkspaceManager,
+	})
+	newLoader.Annotation = pheno
+	return E.Right[error](newLoader)
+}
+
+func (loader *PhenotypeLoader) addPhenotypeRow(
+	pheno *phenotype.PhenotypeAnnotation,
+) (string, error) {
+	var empty string
+	content := F.Pipe7(
+		E.Do[error](pheno),
+		E.Bind(initialPayload, loader.addPheno),
+		E.Bind(phenoIdHandler, phenotypeId),
+		E.Bind(assayIdHandler, assayId),
+		E.Bind(envIdHandler, environmentId),
+		E.Map[error, *PhenotypeLoader](loaderToPayload),
+		E.Chain[error, *PhenotypePayload](marshalPayload),
+		E.Fold(httpapi.OnJSONPayloadError, httpapi.OnJSONPayloadSuccess),
+	)
+	if content.Error != nil {
+		return empty, content.Error
+	}
+	resp := F.Pipe3(
+		loader.createPhenotypeURL(),
+		httpapi.MakeHTTPRequest("POST", bytes.NewBuffer(content.Payload)),
+		R.Map(httpapi.SetHeaderWithJWT(loader.Token)),
+		phenoCreateHTTP,
+	)(context.Background())
+	output := F.Pipe1(
+		resp(),
+		E.Fold(onPhenoCreateFeedbackError, onPhenoCreateFeedbackSuccess),
+	)
+	return output.Msg, output.Err
+}
+
+func loaderToPayload(
+	loader *PhenotypeLoader,
+) *PhenotypePayload {
+	return loader.Payload
+}
+
+func (loader *PhenotypeLoader) createPhenotypeURL() string {
+	return fmt.Sprintf(
+		"https://%s/api/database/rows/table/%d/?user_field_names=true",
+		loader.Host,
+		loader.TableId,
+	)
 }
