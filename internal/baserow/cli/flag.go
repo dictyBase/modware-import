4be4e0cae96e7ba7790d2574c80f2c08d8219cba
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-03-21 11:48:14 -0500

    refactor(cli): move spreadsheetFlag function from strain_flags.go to flag.go for better organization
    The spreadsheetFlag function was moved from strain_flags.go to flag.go.
    This change was made to improve the organization of the codebase. The
    flag.go file is a more appropriate place for this function as it is
    where all the flag-related functions are stored.

--- internal/baserow/cli/flag.go
+++ internal/baserow/cli/flag.go
@@ -66,3 +66,25 @@ func tableCreationFlags() []cli.Flag {
 		},
 	}
 }
+
+func spreadsheetFlag() []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:    "sheet",
+			Aliases: []string{"s"},
+			Usage:   "name of sheet which contains the annotation",
+			Value:   "Strain_Annotations",
+		},
+		&cli.IntFlag{
+			Name:     "table-id",
+			Usage:    "Database table id",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "workspace",
+			Aliases:  []string{"w"},
+			Usage:    "name of the workspace whether the database exists",
+			Required: true,
+		},
+	}
+}
