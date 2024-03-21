45f8d28588826475d0edbe5a4d1407eda061aef9
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-03-20 20:10:07 -0500

    feat(worker.go): add concurrent task processing functionality
    A new file, worker.go, has been added to the concurrent package. This
    file introduces a new feature that allows tasks to be processed
    concurrently. The Task and TaskWrapper types have been defined to
    represent a generic function and its input respectively. The ProcessWork
    function takes a slice of TaskWrappers and processes them concurrently,
    returning a slice of results and any errors that occurred.

---
+++ internal/concurrent/worker.go
@@ -0,0 +1,64 @@
+package concurrent
+
+import (
+	"errors"
+	"sync"
+)
+
+// Task represents a generic function that takes an input of type I and returns an output of type O and an error.
+type Task[I any, O any] func(input I) (O, error)
+
+type TaskWrapper[I any, O any] struct {
+	TaskFunc Task[I, O] // The task function
+	Input    I          // The input for the task function
+}
+
+func ProcessWork[I any, O any](taskSlice []TaskWrapper[I, O]) ([]O, error) {
+	resultCh, errCh := work(taskSlice)
+	results := make([]O, 0) // Add slice to collect results
+	errSlice := make([]error, 0)
+
+	for len(taskSlice) > 0 {
+		select {
+		case err := <-errCh:
+			if err != nil {
+				errSlice = append(errSlice, err)
+			}
+			taskSlice = taskSlice[:len(taskSlice)-1] // Decrease the slice length by 1 for each error received
+		case result, ok := <-resultCh:
+			if !ok {
+				// If the resultCh is closed, exit from the for loop
+				goto FINISH
+			}
+			results = append(results, result)        // Collect result
+			taskSlice = taskSlice[:len(taskSlice)-1] // Decrease the slice length by 1 for each result received
+		}
+	}
+FINISH:
+	return results, errors.Join(errSlice...)
+}
+
+func work[I any, O any](taskSlice []TaskWrapper[I, O]) (chan O, chan error) {
+	resultCh := make(chan O)
+	errCh := make(chan error)
+	var wg sync.WaitGroup
+	// Run each function in a goroutine
+	for _, tsk := range taskSlice {
+		wg.Add(1)
+		go func(trun TaskWrapper[I, O]) {
+			defer wg.Done()
+			result, err := trun.TaskFunc(trun.Input)
+			if err != nil {
+				errCh <- err
+				return
+			}
+			resultCh <- result
+		}(tsk)
+	}
+	go func() {
+		wg.Wait()
+		close(resultCh)
+		close(errCh)
+	}()
+	return resultCh, errCh
+}
