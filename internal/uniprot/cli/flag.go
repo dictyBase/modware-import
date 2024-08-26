095f7dd83fd659665759669366d0aad8b3ade1cc
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-08-26 15:02:38 -0500

    feat(cli/flag.go): add UniprotFlags function and default uniprotURL
    This change introduces a new function `UniprotFlags` to the CLI package,
    which provides flags for configuring the Uniprot URL and Redis service
    connection details. The `uniprotURL` is pre-configured to fetch specific
    data from the Uniprot database, enhancing the ease of use for the
    end-users by providing a ready-to-use URL. The Redis service host and
    port can now be configured via environment variables or command-line
    flags, improving the flexibility and configurability of the application.

--- internal/uniprot/cli/flag.go
+++ internal/uniprot/cli/flag.go
@@ -1,1 +1,37 @@
 package cli
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli/v2"
+)
+
+var uniprotURL = fmt.Sprintf(
+	"%sorganism_id:%d&fields=%s&format=%s&size=%d,`",
+	"https://rest.uniprot.org/uniprotkb/search?query=",
+	44689, "id,xref_dictybase", "json", 500,
+)
+
+func UniprotFlags() []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:    "uniprot-url",
+			Aliases: []string{"u"},
+			Usage:   "URL for fetching Uniprot data",
+			Value:   uniprotURL,
+		},
+		&cli.StringFlag{
+			Name:    "redis-master-service-host",
+			Aliases: []string{"s"},
+			Usage:   "Redis service host address",
+			EnvVars: []string{"REDIS_MASTER_SERVICE_HOST"},
+		},
+		&cli.StringFlag{
+			Name:    "redis-master-service-port",
+			Aliases: []string{"p"},
+			Usage:   "Redis service port",
+			Value:   "6379",
+			EnvVars: []string{"REDIS_MASTER_SERVICE_PORT"},
+		},
+	}
+}
