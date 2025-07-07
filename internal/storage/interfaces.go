08812d42eb68079ef3d62f8333317f61390051ce
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-07-07 17:19:00 -0500

    feat(storage): add FeatureAnnotationStorage interface
    This commit introduces the `FeatureAnnotationStorage` interface, which
    defines the contract for interacting with feature annotation data. This
    interface outlines essential operations such as creating, retrieving,
    updating, and deleting feature annotations, as well as managing
    associated tags and listing annotations by PubMed ID or DOI. This
    abstraction will enable easier testing and allow for different storage
    backends to be implemented in the future.

---
+++ internal/storage/interfaces.go
@@ -0,0 +1,38 @@
+package storage
+
+import (
+	feature "github.com/dictyBase/go-genproto/dictybaseapis/feature_annotation"
+)
+
+// FeatureAnnotationStorage defines the interface for feature annotation storage operations
+type FeatureAnnotationStorage interface {
+	// Create stores a new feature annotation
+	Create(annotation *feature.FeatureAnnotation) error
+
+	// GetByID retrieves a feature annotation by its ID
+	GetByID(id string) (*feature.FeatureAnnotation, error)
+
+	// GetByName retrieves a feature annotation by its name
+	GetByName(name string) (*feature.FeatureAnnotation, error)
+
+	// Update modifies an existing feature annotation
+	Update(id string, annotation *feature.FeatureAnnotation) error
+
+	// Delete removes a feature annotation (soft delete if purge=false)
+	Delete(id string, purge bool) error
+
+	// AddTag adds a tag property to a feature annotation
+	AddTag(id string, tag *feature.TagProperty) error
+
+	// UpdateTag modifies an existing tag in a feature annotation
+	UpdateTag(id string, tagName string, tag *feature.TagProperty) error
+
+	// RemoveTag removes a tag from a feature annotation
+	RemoveTag(id string, tagName string) error
+
+	// ListByPubmedID retrieves feature annotations by PubMed ID
+	ListByPubmedID(pubmedId string) ([]*feature.FeatureAnnotation, error)
+
+	// ListByDOI retrieves feature annotations by DOI
+	ListByDOI(doi string) ([]*feature.FeatureAnnotation, error)
+}
