805619434142d9a2b4a2533604d8354b7edfdc61
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-12-11 20:11:39 -0600

    refactor(stats.go): simplify resource building and query execution logic
    Simplify the resource building by chaining methods directly on `filesql.NewBuilder`.
    Refactor `queryCount` to use `IOE.TryCatch` instead of `F.Pipe2` for cleaner error handling.
    Introduce `generateStats` function to encapsulate the setup logic for better readability in `Run`.

--- internal/gpadstats/stats.go
+++ internal/gpadstats/stats.go
@@ -25,9 +25,9 @@ func builder(config StatsLoaderConfig) IOE.IOEither[error, StatsLoaderConfig] {
 	return IOE.TryCatchError(func() (StatsLoaderConfig, error) {
 		ctx := context.Background()
 		f := config.File
-		builder := filesql.NewBuilder().
-			AddReader(f, "gpad", filesql.FileTypeTSV)
-		validatedBuilder, err := builder.Build(ctx)
+		validatedBuilder, err := filesql.NewBuilder().
+			AddReader(f, "gpad", filesql.FileTypeTSV).
+			Build(ctx)
 		if err != nil {
 			_ = f.Close()
 			return config, err
@@ -54,20 +54,15 @@ func openResources(
 }
 
 func queryCount(config StatsLoaderConfig) IOE.IOEither[error, int] {
-	return F.Pipe2(
-		IOE.TryCatchError(func() (int, error) {
-			var count int
-			err := config.DB.QueryRow("SELECT COUNT(DISTINCT DB_Object_ID) FROM gpad").
-				Scan(&count)
-			return count, err
-		}),
-		IOE.MapLeft[int](func(err error) error {
-			return fmt.Errorf("error in running gpad query %w", err)
-		}),
-		IOE.Map[error](func(ct int) int {
-			return ct
-		}),
-	)
+	return IOE.TryCatch(func() (int, error) {
+		var count int
+		err := config.DB.QueryRow(
+			"SELECT COUNT(DISTINCT DB_Object_ID) FROM gpad",
+		).Scan(&count)
+		return count, err
+	}, func(err error) error {
+		return fmt.Errorf("error in running gpad query %w", err)
+	})
 }
 
 func releaseResources(
@@ -85,16 +80,20 @@ func toEither[ERR, A any](io IOE.IOEither[ERR, A]) E.Either[ERR, A] {
 	return io()
 }
 
+func builderFromFile(file string) IOE.IOEither[error, StatsLoaderConfig] {
+	return F.Pipe2(
+		IOE.Do[error](StatsLoaderConfig{
+			Path: file,
+		}),
+		IOE.Chain(openResources),
+		IOE.Chain(builder),
+	)
+}
+
 func Run(cltx *cli.Context) error {
 	output := F.Pipe2(
 		IOE.Bracket(
-			F.Pipe2(
-				IOE.Do[error](StatsLoaderConfig{
-					Path: cltx.String("file"),
-				}),
-				IOE.Chain(openResources),
-				IOE.Chain(builder),
-			),
+			builderFromFile(cltx.String("file")),
 			queryCount,
 			releaseResources,
 		),
