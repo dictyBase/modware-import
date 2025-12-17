4e6e4a34aaa9198669b235716a0229ada8c7a197
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-12-17 15:43:45 -0600

    refactor(cli): simplify goldenbraid inventory loading error handling
    Introduce a helper struct and functions to encapsulate the result of
    inventory loading and streamline the output handling. This removes
    complex nested error folding logic and makes the flow clearer by
    separating result creation from result presentation.

--- internal/cli/stockcenter/goldenbraid_inventory.go
+++ internal/cli/stockcenter/goldenbraid_inventory.go
@@ -12,39 +12,50 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+type inventoryLoadResult struct {
+	Summary stockcenter.InventoryProcessingSummary
+	Error   error
+}
+
+func onInventoryError(err error) inventoryLoadResult {
+	return inventoryLoadResult{Error: err}
+}
+
+func onInventorySuccess(
+	summary stockcenter.InventoryProcessingSummary,
+) inventoryLoadResult {
+	return inventoryLoadResult{Summary: summary}
+}
+
+func handleInventoryOutput(
+	result inventoryLoadResult,
+	slogger *slog.Logger,
+) error {
+	if result.Error != nil {
+		return result.Error
+	}
+
+	slogger.Info(
+		"GoldenBraid inventory loading summary",
+		"successes", result.Summary.SuccessCount,
+		"errors", result.Summary.ErrorCount,
+	)
+	return nil
+}
+
 func LoadGoldenBraidInventory(cmd *cli.Context) error {
 	handler := logger.GetCliSlogHandler(cmd)
 	slogger := slog.New(handler)
-	elog := E.Logger[error, stockcenter.InventoryProcessingSummary](
-		slog.NewLogLogger(handler, slog.LevelInfo),
-		slog.NewLogLogger(handler, slog.LevelError),
-	)
 
-	return F.Pipe2(
+	output := F.Pipe2(
 		IOE.Bracket(
 			stockcenter.InventoryBuilderFromFile(cmd, slogger),
 			stockcenter.ProcessInventory,
 			stockcenter.ReleaseInventoryResources,
 		),
 		fputil.ToEither[error, stockcenter.InventoryProcessingSummary],
-		E.Fold(
-			func(err error) error {
-				_ = elog(
-					"GoldenBraid inventory loading error",
-				)(
-					E.Left[stockcenter.InventoryProcessingSummary](err),
-				)
-				return err
-			},
-			func(summary stockcenter.InventoryProcessingSummary) error {
-				_ = elog("GoldenBraid inventory loading success")(E.Right[error](summary))
-				slogger.Info(
-					"GoldenBraid inventory loading summary",
-					"successes", summary.SuccessCount,
-					"errors", summary.ErrorCount,
-				)
-				return nil
-			},
-		),
+		E.Fold(onInventoryError, onInventorySuccess),
 	)
+
+	return handleInventoryOutput(output, slogger)
 }
